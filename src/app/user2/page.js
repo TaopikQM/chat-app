a84fcd61c6d9434d7f26cde538b7a7093dd7acda
TaopikQM <taopik61318@gmail.com>
2024-10-11 00:38:14 +0700

    Update page.js

--- src/app/user2/page.js
+++ src/app/user2/page.js
@@ -1,12 +1,8 @@
-import Chat from '../components/Chat';
-import { UserProvider } from '../context/UserContext';
 
-const User2Page = () => {
-    return (
-        <UserProvider>
-            <Chat user={{ id: 'user2', name: 'User 2' }} />
-        </UserProvider>
-    );
-};
-
-export default User2Page;
+export default function Home() {
+  return (
+    <>
+     
+    </>
+  );
+}
