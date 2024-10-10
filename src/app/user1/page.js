9789f08454f8131ec3c9772f6664c688773f89a2
TaopikQM <taopik61318@gmail.com>
2024-10-11 00:13:52 +0700

    Create page.js

---
+++ src/app/user1/page.js
@@ -0,0 +1,12 @@
+import Chat from './components/Chat';
+import { UserProvider } from './context/UserContext';
+
+const User1Page = () => {
+    return (
+        <UserProvider>
+            <Chat user={{ id: 'user1', name: 'User 1' }} />
+        </UserProvider>
+    );
+};
+
+export default User1Page;
