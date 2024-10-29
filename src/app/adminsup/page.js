438715f9317de800e3ac326e97245f8635461001
TaopikQM <taopik61318@gmail.com>
2024-10-30 06:50:41 +0700

    Update page.js

--- src/app/adminsup/page.js
+++ src/app/adminsup/page.js
@@ -5,9 +5,23 @@ import { UserProvider } from '../context/UserContext';
 const User4Page = () => {
     return (
         <UserProvider>
-            <ChatA user={{ id: 'admin', name: 'User gab' }} />
+            <ChatA user={{ id: 'user1', name: 'User gab' }} />
         </UserProvider>
     );
 };
 
 export default User4Page;
+
+
+// import Chat from '../components/Chat';
+// import { UserProvider } from '../context/UserContext';
+
+// const User1Page = () => {
+//     return (
+//         <UserProvider>
+//             <Chat user={{ id: 'user1', name: 'User 1' }} />
+//         </UserProvider>
+//     );
+// };
+
+// export default User1Page;
