8453360629a188e34ffe144ee206195b7e197d91
TaopikQM <taopik61318@gmail.com>
2024-10-30 08:17:29 +0700

    Update ChatA.js

--- src/app/components/ChatA.js
+++ src/app/components/ChatA.js
@@ -216,8 +216,7 @@ const Chat = ({ user }) => {
                         </div>
                         <div className="text-xs text-gray-500 flex justify-end items-center">
                             {msg.timestamp && !isNaN(Number(msg.timestamp))
-                                ? new Date(Number(msg.timestamp)).toLocaleString()  // Convert timestamp and format if valid
-                                : ''  // Placeholder if timestamp is invalid
+                                // Placeholder if timestamp is invalid
                             }
                            
                             {msg.sender === user.id && (
