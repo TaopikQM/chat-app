706cd5f4a60f63dd50b4418b9e05a94f5b2a6b7f
TaopikQM <taopik61318@gmail.com>
2024-11-04 10:28:41 +0700

    Update page.js

--- src/app/adminZ2/page.js
+++ src/app/adminZ2/page.js
@@ -1,1 +1,12 @@
+import adminZ2 from '../components/adminZ2';
+import { UserProvider } from '../context/UserContext';
 
+const adminZ2Page = () => {
+    return (
+        <UserProvider>
+            <Chat user={{ id: 'user2', name: 'User 2' }} />
+        </UserProvider>
+    );
+};
+
+export default adminZ2Page;
