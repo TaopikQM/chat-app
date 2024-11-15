773d091670bc6ba1e59d65e873174c356d64be96
TaopikQM <taopik61318@gmail.com>
2024-11-15 22:17:28 +0700

    Update page.js

--- src/app/user2/page.js
+++ src/app/user2/page.js
@@ -1,10 +1,12 @@
 
 import AddUser from '../components/AddUser';
+import UserTable from '../components/UserTable';
 
 export default function Home() {
   return (
     <>
         <AddUser />
+        <UserTable />
     </>
   );
 }
