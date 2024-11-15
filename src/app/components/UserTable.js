9e54aa19de63ab9ea2ed85c9a79aac0c09d1966d
TaopikQM <taopik61318@gmail.com>
2024-11-16 00:44:23 +0700

    Update UserTable.js

--- src/app/components/UserTable.js
+++ src/app/components/UserTable.js
@@ -194,10 +194,21 @@ const UsersTable = () => {
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg ">
                 <div className="flex justify-between items-center px-4 py-3">
                     <div className="text-xl font-bold mb-4">Daftar Pengguna</div>
-                    <div className="relative ">
-                        <button onClick={toggleForm} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 m-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add</button>
+                    <div className="relative">
+                        <button
+                            onClick={toggleForm}
+                            type="button"
+                            className={`text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 m-2 focus:outline-none ${
+                                showForm
+                                    ? "bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                                    : "bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                            }`}
+                        >
+                            {showForm ? "Kembali" : "Add"}
+                        </button>
                         {showForm && <AddUser />}
                     </div>
+
                   
                 </div>
                 <div className="flex justify-between  px-4 py-3">
