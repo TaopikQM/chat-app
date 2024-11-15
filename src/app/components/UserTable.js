85982b825291fed30039356594765b2864beb1c9
TaopikQM <taopik61318@gmail.com>
2024-11-16 00:25:33 +0700

    Update UserTable.js

--- src/app/components/UserTable.js
+++ src/app/components/UserTable.js
@@ -63,9 +63,9 @@ const UsersTable = () => {
                     };
 
                     usersArray.forEach(user => {
-                        if (user.status === 'active') {
+                        if (user.status === 'Active') {
                             totals.active++;
-                        } else if (user.status === 'inactive') {
+                        } else if (user.status === 'Inactive') {
                             totals.inactive++;
                         }
 
@@ -272,7 +272,7 @@ const UsersTable = () => {
                                    
                                     <td className="py-2 px-4 text-center">
                                           <button onClick={() => handleChangeStatusWithConfirmation(user)}>
-                                            {user.status === 'active' ? 
+                                            {user.status === 'Active' ? 
                                             <button className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Active</button> : 
                                             <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Inactive</button>}
                                         </button>
