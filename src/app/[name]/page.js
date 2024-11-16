351c6b5e14d59c833c248e101ec4a3a1feb324a1
TaopikQM <taopik61318@gmail.com>
2024-11-17 00:05:30 +0700

    Update page.js

--- src/app/[name]/page.js
+++ src/app/[name]/page.js
@@ -67,7 +67,7 @@ const UserPage = () => {
     // Ensure userData exists before accessing its properties
     return (
         <UserProvider>
-            <Chat0 user={{ id: userName, name: userData?.name }} />
+            <Chat0 user={{ name: userData?.name }} />
         </UserProvider>
     );
 };
