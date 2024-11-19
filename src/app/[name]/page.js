6ae47e779c2d1bbcfa59e7985ee8fb24d991e4cd
TaopikQM <taopik61318@gmail.com>
2024-11-19 14:35:51 +0700

    Update page.js

--- src/app/[name]/page.js
+++ src/app/[name]/page.js
@@ -112,7 +112,7 @@ const UserPage = () => {
                            
                             <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
                               <div class="px-4 py-3" role="none">
-                                <p class="text-sm text-gray-900 dark:text-white" role="none">
+                                <p class="text-sm text-gray-900 dark:text-black" role="none">
                                   {userData.name}
                                 </p>
                               </div>
