7dd047045bcccb421b730df32070362a2db0008e
TaopikQM <taopik61318@gmail.com>
2024-11-22 08:21:05 +0700

    Update page.js

--- src/app/[name]/page.js
+++ src/app/[name]/page.js
@@ -251,7 +251,7 @@ const UserPage = () => {
                 <div className="flex-1 p-4">
                     {selectedUser ? (
                          <div>
-                            <div>
+                            <div className="flex">
                               <h2 className="text-xl font-bold">
                                 Obrolan dengan {selectedUser.name}
                               </h2>
@@ -260,77 +260,78 @@ const UserPage = () => {
                                   Terakhir terlihat: {lastSeen}
                                 </p>
                               )}
-                            </div>
-                            {/* Dropdown Menu */}
-                            <div className="relative">
-                              <button
-                                id="dropdownMenuIconButton"
-                                onClick={toggleDropdown}
-                                className="inline-flex self-center items-center p-2 text-sm font-medium text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-800 dark:focus:ring-gray-600"
-                                type="button"
-                              >
-                                <svg
-                                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                                  aria-hidden="true"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="currentColor"
-                                  viewBox="0 0 4 15"
-                                >
-                                  <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
-                                </svg>
-                              </button>
-                              {dropdownOpen && (
-                                <div
-                                  id="dropdownDots"
-                                  className="absolute right-0 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-40 dark:bg-gray-700 dark:divide-gray-600"
-                                >
-                                  <ul
-                                    className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                                    aria-labelledby="dropdownMenuIconButton"
+                           
+                                {/* Dropdown Menu */}
+                                <div className="relative">
+                                  <button
+                                    id="dropdownMenuIconButton"
+                                    onClick={toggleDropdown}
+                                    className="inline-flex self-center items-center p-2 text-sm font-medium text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-800 dark:focus:ring-gray-600"
+                                    type="button"
                                   >
-                                    <li>
-                                      <a
-                                        href="#"
-                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                    <svg
+                                      className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                                      aria-hidden="true"
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      fill="currentColor"
+                                      viewBox="0 0 4 15"
+                                    >
+                                      <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
+                                    </svg>
+                                  </button>
+                                  {dropdownOpen && (
+                                    <div
+                                      id="dropdownDots"
+                                      className="absolute right-0 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-40 dark:bg-gray-700 dark:divide-gray-600"
+                                    >
+                                      <ul
+                                        className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                                        aria-labelledby="dropdownMenuIconButton"
                                       >
-                                        Reply
-                                      </a>
-                                    </li>
-                                    <li>
-                                      <a
-                                        href="#"
-                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                      >
-                                        Forward
-                                      </a>
-                                    </li>
-                                    <li>
-                                      <a
-                                        href="#"
-                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                      >
-                                        Copy
-                                      </a>
-                                    </li>
-                                    <li>
-                                      <a
-                                        href="#"
-                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                      >
-                                        Report
-                                      </a>
-                                    </li>
-                                    <li>
-                                      <a
-                                        href="#"
-                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                      >
-                                        Delete
-                                      </a>
-                                    </li>
-                                  </ul>
+                                        <li>
+                                          <a
+                                            href="#"
+                                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                          >
+                                            Reply
+                                          </a>
+                                        </li>
+                                        <li>
+                                          <a
+                                            href="#"
+                                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                          >
+                                            Forward
+                                          </a>
+                                        </li>
+                                        <li>
+                                          <a
+                                            href="#"
+                                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                          >
+                                            Copy
+                                          </a>
+                                        </li>
+                                        <li>
+                                          <a
+                                            href="#"
+                                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                          >
+                                            Report
+                                          </a>
+                                        </li>
+                                        <li>
+                                          <a
+                                            href="#"
+                                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                          >
+                                            Delete
+                                          </a>
+                                        </li>
+                                      </ul>
+                                    </div>
+                                  )}
                                 </div>
-                              )}
                             </div>
                             
                                                              
