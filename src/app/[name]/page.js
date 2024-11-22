4d35e911fa50f558667e6f0fcde8a6a4be69bc7b
TaopikQM <taopik61318@gmail.com>
2024-11-22 07:48:58 +0700

    Update page.js

--- src/app/[name]/page.js
+++ src/app/[name]/page.js
@@ -17,7 +17,6 @@ const UserPage = () => {
     const [lastSeen, setLastSeen] = useState("");
     const [selectedFiles, setSelectedFiles] = useState([]); // State untuk menyimpan file yang dipilih
    
-
     useEffect(() => {
         const fetchUserData = async () => {
             const pathParts = window.location.pathname.split("/");
@@ -220,11 +219,7 @@ const UserPage = () => {
     if (!userActive || !userData) {
         return <p>User not found or inactive.</p>;
     }
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
-  };
+    
 
     return (
         <UserProvider>
@@ -261,77 +256,7 @@ const UserPage = () => {
                                 </p>
                               )}
                             </div>
-                            {/* Dropdown Menu */}
-                            <div className="relative">
-                              <button
-                                id="dropdownMenuIconButton"
-                                onClick={toggleDropdown}
-                                className="inline-flex self-center items-center p-2 text-sm font-medium text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-800 dark:focus:ring-gray-600"
-                                type="button"
-                              >
-                                <svg
-                                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                                  aria-hidden="true"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="currentColor"
-                                  viewBox="0 0 4 15"
-                                >
-                                  <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
-                                </svg>
-                              </button>
-                              {dropdownOpen && (
-                                <div
-                                  id="dropdownDots"
-                                  className="absolute right-0 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-40 dark:bg-gray-700 dark:divide-gray-600"
-                                >
-                                  <ul
-                                    className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                                    aria-labelledby="dropdownMenuIconButton"
-                                  >
-                                    <li>
-                                      <a
-                                        href="#"
-                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                      >
-                                        Reply
-                                      </a>
-                                    </li>
-                                    <li>
-                                      <a
-                                        href="#"
-                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                      >
-                                        Forward
-                                      </a>
-                                    </li>
-                                    <li>
-                                      <a
-                                        href="#"
-                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                      >
-                                        Copy
-                                      </a>
-                                    </li>
-                                    <li>
-                                      <a
-                                        href="#"
-                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                      >
-                                        Report
-                                      </a>
-                                    </li>
-                                    <li>
-                                      <a
-                                        href="#"
-                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                      >
-                                        Delete
-                                      </a>
-                                    </li>
-                                  </ul>
-                                </div>
-                              )}
-                            </div>
+                            
                                                              
                             <div className="border p-4 h-[400px] overflow-y-scroll">
                                 {messages.map((msg, index) => (
