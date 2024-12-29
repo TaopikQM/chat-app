af0f4be30fbcca4b85b89881f8ebc3510f08e9ec
TaopikQM <taopik61318@gmail.com>
2024-12-30 06:34:20 +0700

    Create page.js

---
+++ src/app/gallery/page.js
@@ -0,0 +1,12 @@
+// import UploadData from '../components/UploadData';
+import Gallery from '../components/gallery';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Manajemen Data Kehadiran</h1>
+      // <UploadData />
+      <Gallery />
+    </div>
+  );
+}
