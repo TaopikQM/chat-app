cecb75a0466a07ac28ab7601ce9598efa10f21c6
TaopikQM <taopik61318@gmail.com>
2024-12-30 09:33:15 +0700

    Update page.js

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,30 +1,12 @@
-import FilePreview  from '../components/FilePreview';
-import Gallery from '../components/Gallery';
+
 import GalleryU from '../components/GalleryU';
 // Example usage
 
 export default function HomePage() {
-    const files = [
-    "https://firebasestorage.googleapis.com/v0/b/env-sib.appspot.com/o/chatFiles%2F1693183058_c6b5a9f146cfab9cc063%20(1).jpg?alt=media&token=8b67096b-a2d4-47b2-8cc3-4b03cc51efe7",
-    "https://firebasestorage.googleapis.com/v0/b/env-sib.appspot.com/o/chatFiles%2F-O6YejEikTGc9L8iJtLY.mp4?alt=media&token=9fd37e31-2e11-418d-a8c4-aca1be65833a"
-];
+   
   return (
     <div>
       <Gallery />
-    <h1>INI GALERI UTAMA</h1>
-      <GalleryU />
-     
-
-            {/* Video file */}
-            <div className="file-preview">
-                <video className="w-full" controls>
-                    <source
-                        src="https://firebasestorage.googleapis.com/v0/b/env-sib.appspot.com/o/chatFiles%2F-O6YejEikTGc9L8iJtLY.mp4?alt=media&token=9fd37e31-2e11-418d-a8c4-aca1be65833a"
-                        type="video/mp4"
-                    />
-                    Your browser does not support the video tag.
-                </video>
-            </div>
     </div>
   );
 }
