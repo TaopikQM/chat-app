93a59e86cd4adbd91637642e34365ef2b3e2c739
TaopikQM <taopik61318@gmail.com>
2024-12-30 08:33:10 +0700

    Update page.js

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,10 +1,12 @@
 // import UploadData from '../components/UploadData';
 import Gallery from '../components/Gallery';
+import GalleryU from '../components/GalleryU';
 
 export default function HomePage() {
   return (
     <div>
       <Gallery />
+      <GalleryU />
     </div>
   );
 }
