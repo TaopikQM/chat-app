f307e37f8471c7f691483d0b12c2cc986c201b93
TaopikQM <taopik61318@gmail.com>
2025-01-18 04:17:12 +0700

    Create page.js

---
+++ src/app/JSholat/page.js
@@ -0,0 +1,10 @@
+// pages/index.js
+import PrayerTimes from "../components/PrayerTimes";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <PrayerTimes />
+    </div>
+  );
+}
