dcc4b0fed4963562d2202f11bc5bad43e14d42fe
TaopikQM <taopik61318@gmail.com>
2025-01-22 13:26:45 +0700

    Create page.js

---
+++ src/app/JadwalShalat/page.js
@@ -0,0 +1,10 @@
+import JadwalShalat from '../components/JadwalShalat';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Jadwal Shalat</h1>
+      <JadwalShalat />
+    </div>
+  );
+}
