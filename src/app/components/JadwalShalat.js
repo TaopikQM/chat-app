158ce76572005b85ad321616c32aa3a3f24cd767
TaopikQM <taopik61318@gmail.com>
2025-01-22 18:23:32 +0700

    Update JadwalShalat.js

--- src/app/components/JadwalShalat.js
+++ src/app/components/JadwalShalat.js
@@ -14,6 +14,13 @@ const JadwalShalat = () => {
   const [jadwalShalat, setJadwalShalat] = useState([]);
   const [loading, setLoading] = useState(false);
 
+
+  const bulanList = [
+    "Januari", "Februari", "Maret", "April", "Mei", "Juni",
+    "Juli", "Agustus", "September", "Oktober", "November", "Desember"
+  ];
+
+  
   // Ambil data provinsi dari Firebase
   useEffect(() => {
     fetch(provinsiURL)
@@ -58,21 +65,30 @@ const JadwalShalat = () => {
   }, [provinsi]);
 
   const getKabupaten = async (prov) => {
-    try {
-      const response = await fetch(`${host}ajax/getKabkoshalat`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams({ x: prov }),
-      });
+  try {
+    const response = await fetch(`${host}ajax/getKabkoshalat`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Cookie": "bimasislam_session=a%3A5%3A%7Bs%3A10%3A%22session_id%22%3Bs%3A32%3A%22539ea43a3db0c096e0cc9e215ff36886%22%3Bs%3A10%3A%22ip_address%22%3Bs%3A13%3A%22114.79.16.159%22%3Bs%3A10%3A%22user_agent%22%3Bs%3A111%3A%22Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F131.0.0.0+Safari%2F537.36%22%3Bs%3A13%3A%22last_activity%22%3Bi%3A1737544359%3Bs%3A9%3A%22user_data%22%3Bs%3A0%3A%22%22%3B%7Db9f9372250887893737218d575da17dd; _ga_W825VCQ3Z3=GS1.1.1737544527.5.1.1737544610.0.0.0; _ga=GA1.1.1220341070.1737516614; PHPSESSID=8b71dep0r9n5vvefqogpllin64"
+      },
+      body: new URLSearchParams({ x: prov }),
+      credentials: "include", // Mengizinkan pengiriman cookies
+    });
 
-      const data = await response.text();
-      setListKabupaten(data ? JSON.parse(data) : []);
-    } catch (error) {
-      console.error('Error fetching kabupaten:', error);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
-  };
+
+    const textData = await response.text();
+    const jsonData = textData ? JSON.parse(textData) : [];
+
+    setListKabupaten(jsonData);
+  } catch (error) {
+    console.error("Error fetching kabupaten:", error);
+  }
+};
+
 
   const loadJadwalShalat = async () => {
     setLoading(true);
