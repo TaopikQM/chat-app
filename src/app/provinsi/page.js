8c7f5e98bde2db1f08177520a3a4e7e594486d01
TaopikQM <taopik61318@gmail.com>
2025-01-22 11:23:09 +0700

    Update page.js

--- src/app/provinsi/page.js
+++ src/app/provinsi/page.js
@@ -1,10 +1,10 @@
 // pages/index.js
-import PrayerTimes from "../components/ProvinsiPulauForm ";
+import PrayerTimes from "../components/ProvinsiPulauForm";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 p-4">
-      <ProvinsiPulauForm  />
+      <ProvinsiPulauForm />
     </div>
   );
 }
