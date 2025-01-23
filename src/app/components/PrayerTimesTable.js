d3fb9f063ef3f782af4e46c4021ed1a5457c9546
TaopikQM <taopik61318@gmail.com>
2025-01-23 22:41:18 +0700

    Update PrayerTimesTable.js

--- src/app/components/PrayerTimesTable.js
+++ src/app/components/PrayerTimesTable.js
@@ -85,7 +85,7 @@ export default function PrayerTimesTable() {
               <tr key={index} className={`text-center ${isTodayRow ? "bg-yellow-200 font-bold" : ""}`}>
                   <td className="border border-gray-300 p-2">
                     {day.date.gregorian.date} 
-                    {isTodayRow && <span className="ml-2 text-red-600 font-semibold">Hari Ini</span>}
+                    {isTodayRow && }
                   </td>
                 <td className="border border-gray-300 p-2">{convertToWIB(day.timings.Imsak)}</td>
                 <td className="border border-gray-300 p-2">{convertToWIB(day.timings.Fajr)}</td>
@@ -105,3 +105,6 @@ export default function PrayerTimesTable() {
     </div>
   );
 }
+
+
+//<span className="ml-2 text-red-600 font-semibold">Hari Ini</span>
