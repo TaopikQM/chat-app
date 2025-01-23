b797a543fdc6712a7069f3e64ef97be6cf10b72d
TaopikQM <taopik61318@gmail.com>
2025-01-23 21:17:18 +0700

    Create page.js

---
+++ src/app/PrayerTimesTable/page.js
@@ -0,0 +1,9 @@
+import PrayerTimesTable from "../components/PrayerTimesTable";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <PrayerTimesTable />
+    </div>
+  );
+}
