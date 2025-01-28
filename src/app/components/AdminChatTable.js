c502f7e3bd6d3144e559938e2c88b41b92fd8533
TaopikQM <taopik61318@gmail.com>
2025-01-28 18:54:45 +0700

    Update AdminChatTable.js

--- src/app/components/AdminChatTable.js
+++ src/app/components/AdminChatTable.js
@@ -42,7 +42,7 @@ const AdminChatTable = () => {
                 <td className="border border-gray-300 px-4 py-2 text-center">{msg.penerima}</td>
                 <td className="border border-gray-300 px-4 py-2">{msg.pesan}</td>
                 {/* File yang dikirim */}
-                  <td className="border border-gray-300 px-4 py-2 text-center">
+                  <td className="border border-gray-300 px-4 py-2 ">
                     {msg.files && msg.files.length > 0 ? (
                       <div className="flex flex-col gap-1">
                         {msg.files.map((file, fileIndex) => (
