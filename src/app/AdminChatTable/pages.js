ad73742fe857292310e19da8d8bde0d730ca16cd
TaopikQM <taopik61318@gmail.com>
2025-01-28 11:21:17 +0700

    Create pages.js

---
+++ src/app/AdminChatTable/pages.js
@@ -0,0 +1,9 @@
+import AdminChatTable from "../components/AdminChatTable";
+
+export default function AdminPage() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <AdminChatTable />
+    </div>
+  );
+}
