768519fc45b3073af645bed20cd0601bacee1c44
TaopikQM <taopik61318@gmail.com>
2025-02-19 12:01:38 +0700

    Create page.js

---
+++ src/app/maps/page.js
@@ -0,0 +1,10 @@
+import LiveTrackingMap from '../components/LiveTrackingMap';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Jadwal Shalat</h1>
+      <LiveTrackingMap />
+    </div>
+  );
+}
