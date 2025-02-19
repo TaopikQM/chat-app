6d2c31e7ddbc2a1bd0161eeaa0525152174d093b
TaopikQM <taopik61318@gmail.com>
2025-02-19 12:44:44 +0700

    Update page.js

--- src/app/maps/page.js
+++ src/app/maps/page.js
@@ -1,10 +1,10 @@
-import LiveTrackingMap from '../components/LiveTrackingMap';
+// import LiveTrackingMap from '../components/LiveTrackingMap';
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Jadwal Shalat</h1>
-      <LiveTrackingMap />
-    </div>
-  );
-}
+// export default function Home() {
+//   return (
+//     <div>
+//       <h1>Jadwal Shalat</h1>
+//       <LiveTrackingMap />
+//     </div>
+//   );
+// }
