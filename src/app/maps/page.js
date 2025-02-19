5482cca9a0b728c97c60de41e42ec8d9b380c667
TaopikQM <taopik61318@gmail.com>
2025-02-19 12:54:17 +0700

    Update page.js

--- src/app/maps/page.js
+++ src/app/maps/page.js
@@ -1,8 +1,8 @@
 import dynamic from "next/dynamic";
 
 // Import `MapView` secara dinamis agar tidak di-render di server
-const Maps = dynamic(() => import("../components/Maps"), { ssr: false });
-import LiveTrack from "../components/LiveTrack"; // Jalankan tracking lokasi di background
+const Maps = dynamic(() => import("@/components/Maps"), { ssr: false });
+import LiveTrack = dynamic(() => import("@/components/LiveTrack"), { ssr: false }); // Jalankan tracking lokasi di background
 
 export default function Home() {
   return (
