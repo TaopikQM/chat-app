a2e287c6a461c86f5fe501d9c2fa758786aa0960
TaopikQM <taopik61318@gmail.com>
2025-03-01 18:37:18 +0700

    Update page.js

--- src/app/maps/page.js
+++ src/app/maps/page.js
@@ -1,24 +1,24 @@
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 
-// Import `MapView` secara dinamis agar tidak di-render di server
-const Maps = dynamic(() => import("@/components/Maps"), { ssr: false });
-const LiveTrack = dynamic(() => import("@/components/LiveTrack"), { ssr: false });// Jalankan tracking lokasi di background
-
-export default function Home() {
-  return (
-    <>
-      <LiveTrack /> {/* Kirim lokasi ke Firebase di background */}
-      <Maps /> {/* Tampilkan peta dengan data dari Firebase */}
-    </>
-  );
-}
-// import LiveTrackingMap from '../components/LiveTrackingMap';
+// // Import `MapView` secara dinamis agar tidak di-render di server
+// const Maps = dynamic(() => import("@/components/Maps"), { ssr: false });
+// const LiveTrack = dynamic(() => import("@/components/LiveTrack"), { ssr: false });// Jalankan tracking lokasi di background
 
 // export default function Home() {
 //   return (
-//     <div>
-//       <h1>Jadwal Shalat</h1>
-//       <LiveTrackingMap />
-//     </div>
+//     <>
+//       <LiveTrack /> {/* Kirim lokasi ke Firebase di background */}
+//       <Maps /> {/* Tampilkan peta dengan data dari Firebase */}
+//     </>
 //   );
 // }
+// // import LiveTrackingMap from '../components/LiveTrackingMap';
+
+// // export default function Home() {
+// //   return (
+// //     <div>
+// //       <h1>Jadwal Shalat</h1>
+// //       <LiveTrackingMap />
+// //     </div>
+// //   );
+// // }
