1b9f2c46cbdf1ce713542fcb5cc025e24691aa0a
TaopikQM <taopik61318@gmail.com>
2025-03-01 19:36:16 +0700

    Update MapView.js

--- src/app/components/MapView.js
+++ src/app/components/MapView.js
@@ -1,4 +1,4 @@
-// "use client";
+"use client";
 // import { useEffect, useState } from "react";
 // import { MapContainer, TileLayer, Polyline, Marker } from "react-leaflet";
 // import "leaflet/dist/leaflet.css";
