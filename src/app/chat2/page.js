f6998f1a2bedfd7d64e6ecc82744d68e4493adbb
TaopikQM <taopik61318@gmail.com>
2025-03-01 19:58:57 +0700

    Update page.js

--- src/app/chat2/page.js
+++ src/app/chat2/page.js
@@ -7,7 +7,7 @@ import {useEffect, useState } from "react";
 
 // import { rtdb, ref, update, serverTimestamp } from "../../config/firebase";
 
-import { rtdb, storage } from "../../config/firebase";
+import { database, storage } from "../../config/firebase";
 import { ref as databaseRef, push, update,set ,onValue,serverTimestamp } from "firebase/database";
 const ChatPage = () => {
   const [currentUser] = useState("user1"); // Gantilah dengan ID pengguna yang sesuai
@@ -16,7 +16,7 @@ const ChatPage = () => {
   const [replyMessage, setReplyMessage] = useState(null); // ✅ Reply Message
 
   useEffect(() => {
-    const userRef = databaseRef(rtdb, `pengguna/${currentUser}`);
+    const userRef = databaseRef(database, `pengguna/${currentUser}`);
     
 
     // Set pengguna online saat masuk
