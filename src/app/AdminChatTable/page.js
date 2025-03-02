4606618c59bc10035975a46e1a27b050fba21695
TaopikQM <taopik61318@gmail.com>
2025-03-02 22:16:15 +0700

    Update page.js

--- src/app/AdminChatTable/page.js
+++ src/app/AdminChatTable/page.js
@@ -1,9 +1,23 @@
 import AdminChatTable from "../components/AdminChatTable";
+import LAdminChatTable from "../components/LAdminChatTable";
+import UsersChatTable from "../components/UsersChatTable";
+import LUsersChatTable from "../components/LUsersChatTable";
 
 export default function AdminPage() {
   return (
+    <>
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <AdminChatTable />
     </div>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <LAdminChatTable />
+    </div>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <UsersChatTable />
+    </div>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <LUsersChatTable />
+    </div>
+    </>
   );
 }
