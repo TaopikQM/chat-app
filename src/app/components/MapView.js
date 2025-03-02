5f9c2e8de013e9286febad1742dcee786b9fe3d2
TaopikQM <taopik61318@gmail.com>
2025-03-02 19:57:10 +0700

    Update MapView.js

--- src/app/components/MapView.js
+++ src/app/components/MapView.js
@@ -8,7 +8,7 @@ import { ref, onValue } from "firebase/database";
 import L from "leaflet";
 
 const customIcon = new L.Icon({
-  iconUrl: "/Icon/gridicons_location.svg",
+  iconUrl: "/assets/Icon/gridicons_location.svg",
   iconSize: [32, 32],
   iconAnchor: [16, 32],
   popupAnchor: [0, -32],
