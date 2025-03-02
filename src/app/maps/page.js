34de76dc68328420788d5d0b286c0490f3c5c983
TaopikQM <taopik61318@gmail.com>
2025-03-02 18:19:59 +0700

    Update and rename pags.js to page.js

--- src/app/maps/page.js
+++ src/app/maps/page.js
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 
-const MapView = dynamic(() => import("../../components/MapView"), { ssr: false });
+const MapView = dynamic(() => import("../components/MapView"), { ssr: false });
 
 export default function Home() {
   return (
