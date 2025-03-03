a0795c3d731276cb6d20e3c8e6c7f33bc39e9f93
TaopikQM <taopik61318@gmail.com>
2025-03-03 12:43:07 +0700

    Update page.js

--- src/app/user1/page.js
+++ src/app/user1/page.js
@@ -61,7 +61,7 @@ const ChatPage = () => {
         status: "update_lastSeen",
         timestamp: serverTimestamp(),
       });
-    }, 20000);
+    }, 50000);
 
     window.addEventListener("beforeunload", handleDisconnect);
     return () => {
