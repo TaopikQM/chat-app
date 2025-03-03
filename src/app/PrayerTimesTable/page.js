fb8f0d13851912125adbf8eafacfe929c734acaf
TaopikQM <taopik61318@gmail.com>
2025-03-04 04:08:25 +0700

    Update page.js

--- src/app/PrayerTimesTable/page.js
+++ src/app/PrayerTimesTable/page.js
@@ -1,9 +1,15 @@
 import PrayerTimesTable from "../components/PrayerTimesTable";
+import timehijr from "../components/timehijr";
 
 export default function Home() {
   return (
+    <>
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <PrayerTimesTable />
     </div>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <timehijr />
+    </div>
+    </>
   );
 }
