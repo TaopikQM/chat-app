ae2800153be0cfaca79d744732c33633304c5a05
TaopikQM <taopik61318@gmail.com>
2025-03-04 16:57:55 +0700

    Update page.js

--- src/app/PrayerTimesTable/page.js
+++ src/app/PrayerTimesTable/page.js
@@ -8,6 +8,7 @@ export default function Home() {
       <PrayerTimesTable />
     </div>
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <TimeHijr />
     </div>
     </>
   );
