241d154414e2bd575a3fcd95101451d7abdeb5b5
TaopikQM <taopik61318@gmail.com>
2025-03-07 03:00:07 +0700

    Create page.js

---
+++ src/app/JHijrS/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import JSholat from "../components/JSholat";
+
+export default function JSHPage() {
+  return (
+    <>
+   
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <JSholat />
+    </div>
+    </>
+  );
+}
