b63430b7eec1b5f0a7310e37d2d7febda60567bd
TaopikQM <taopik61318@gmail.com>
2025-03-08 02:32:21 +0700

    Update page.js

--- src/app/4user/page.js
+++ src/app/4user/page.js
@@ -2,7 +2,7 @@
 import Chats from '../components/Chats';
 import { UserProvider } from '../context/UserContext';
 
-const Syifa2 = () => {
+const Syifa2 = () => { 
     return (
         <UserProvider>
             <Chats user={{ id: 'user2', name: 'User 2' }} />
