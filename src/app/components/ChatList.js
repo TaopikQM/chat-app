009e8eb8f4828978edbf2549d23b4cc6e911d92f
TaopikQM <taopik61318@gmail.com>
2025-03-08 22:19:29 +0700

    Update ChatList.js

--- src/app/components/ChatList.js
+++ src/app/components/ChatList.js
@@ -32,15 +32,15 @@ const [previousPage, setPreviousPage] = useState(1);
         .filter((msg) =>
         msg.status === "ACTIVE" && // Hanya ambil pesan yang aktif
         (
-            (msg.pengirim === user1 && msg.penerima === user2) ||
+            (msg.pengirim === user1 && msg.penerima === user2) || 
             (msg.pengirim === user2 && msg.penerima === user1)
+        ) && 
+        (
+            msg.pengirim === user1 || msg.pengirim === user2 || msg.onUSer === "ON" // Pengirim tetap lihat, penerima hanya kalau ON
         ) &&
         (
-            msg.pengirim === user1 || msg.pengirim === user2 || msg.onUSer === "ON" // Pengirim tetap bisa lihat, penerima hanya kalau ON
-        ) &&
-        (
-            !(msg.onUSer === "OFF" && msg.penerima === user1) && // Sembunyikan untuk user1 jika dia penerima
-            !(msg.onUSer === "OFF" && msg.penerima === user2)    // Sembunyikan untuk user2 jika dia penerima
+            !(msg.onUSer === "OFF" && msg.penerima === user1 && msg.pengirim !== user1) && // Sembunyikan untuk user1 jika dia penerima
+            !(msg.onUSer === "OFF" && msg.penerima === user2 && msg.pengirim !== user2)    // Sembunyikan untuk user2 jika dia penerima
         )
         //   msg.status === "ACTIVE" && 
         //    // msg.onUser !== "OFF" && 
