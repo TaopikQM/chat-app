49250c3ffbab925a139e50004355bc86694ee0f1
TaopikQM <taopik61318@gmail.com>
2025-03-08 17:05:38 +0700

    Update JSholat.js

--- src/app/components/JSholat.js
+++ src/app/components/JSholat.js
@@ -420,7 +420,7 @@ export default function LUsersChatTable() {
       return;
     }
 
-    const prayerType = newHour  >= 1 && currentHour < 6 ? "Fajr" : "Normal"; 
+    const prayerType = newHour  >= 1 && newHour < 6 ? "Fajr" : "Normal"; 
 
     setTriggers([...triggers, { hour: newHour, minute: newMinute }]);
     setTriggerHour("");
