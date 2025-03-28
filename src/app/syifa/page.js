1c429ec17c4001951a9b2cb415bb91606f95ddfc
TaopikQM <taopik61318@gmail.com>
2025-03-29 05:44:13 +0700

    Update page.js

--- src/app/syifa/page.js
+++ src/app/syifa/page.js
@@ -15,23 +15,15 @@ const ChatPage = () => {
   
   const [replyMessage, setReplyMessage] = useState(null); // ✅ Reply Message
 
-  // ✅ Pindahkan handleTyping ke luar useEffect agar bisa diakses global
-  const handleTyping = () => {
-    if (!currentUser) return;
-    const userTypingRef = databaseRef(database, `typingStatus/${currentUser}`);
-
-    update(userTypingRef, { isTyping: true });
-
-    // Hapus status mengetik setelah 2 detik user tidak mengetik
-    setTimeout(() => {
-      update(userTypingRef, { isTyping: false });
-    }, 2000);
-  };
+ 
+  let typingTimeout;
   useEffect(() => {
      
     const userRef = databaseRef(database, `pengguna/${currentUser}`);
 
     const logsRef = databaseRef(database, `logs_pengguna/${currentUser}`);
+    
+    const typingRef = databaseRef(database, `typingStatus/${currentUser}`);
 
     // Set pengguna online saat masuk
     update(userRef, {
@@ -46,6 +38,17 @@ const ChatPage = () => {
       status: "online",
       timestamp: serverTimestamp(),
     });
+
+    // ✅ Event mengetik otomatis
+    const handleTyping = () => {
+      if (!currentUser) return;
+      update(typingRef, { isTyping: true });
+
+      clearTimeout(typingTimeout);
+      typingTimeout = setTimeout(() => {
+        update(typingRef, { isTyping: false });
+      }, 2000);
+    };
 
     // Set pengguna offline saat keluar
     const handleDisconnect = () => {
