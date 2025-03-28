c2c00b154b4106ef5df02de2a28fc6c4519a99bd
TaopikQM <taopik61318@gmail.com>
2025-03-28 18:11:01 +0700

    Update ChatMessage.js

--- src/app/components/ChatMessage.js
+++ src/app/components/ChatMessage.js
@@ -109,16 +109,7 @@ const ChatMessage = ({ message, user1, openDropdownId, setOpenDropdownId, setRep
       <div className={`flex ${isSender ? "justify-end" : "justify-start"}`}>
         <div className="items-start space-x-2">
  
- {/* Tampilkan tanggal jika pesan adalah pesan pertama di hari baru */}
-    {index === 0 || format(new Date(message.timestamp), "dd-MM-yyyy") !== format(new Date(messages[index - 1].timestamp), "dd-MM-yyyy") ? (
-      <div className="flex items-center my-4">
-        <hr className="flex-grow border-t border-gray-300" />
-        <span className="mx-4 text-gray-500 text-xs font-semibold">
-          {format(new Date(message.timestamp), "dd MMMM yyyy")}
-        </span>
-        <hr className="flex-grow border-t border-gray-300" />
-      </div>
-    ) : null}
+
           
           {message.replyTo && (
             <div
