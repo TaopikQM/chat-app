8fadcb691afa1912aae5c1b50a4a6a1188d3997c
TaopikQM <taopik61318@gmail.com>
2025-03-29 05:41:39 +0700

    Update page.js

--- src/app/4user/page.js
+++ src/app/4user/page.js
@@ -14,18 +14,9 @@ const ChatPage = () => {
   const [chatWith] = useState("user4"); // ID pengguna tujuan
 
   const [replyMessage, setReplyMessage] = useState(null); // ✅ Reply Message
-  // ✅ Pindahkan handleTyping ke luar useEffect agar bisa diakses global
-  const handleTyping = () => {
-    if (!chatWith) return;
-    const userTypingRef = databaseRef(database, `typingStatus/${chatWith}`);
 
-    update(userTypingRef, { isTyping: true });
-
-    // Hapus status mengetik setelah 2 detik user tidak mengetik
-    setTimeout(() => {
-      update(userTypingRef, { isTyping: false });
-    }, 2000);
-  };
+  
+  let typingTimeout;
 
   // useEffect(() => {
   //   const userRef = databaseRef(database, `pengguna/${chatWith}`);
@@ -62,6 +53,8 @@ const ChatPage = () => {
     const userRef = databaseRef(database, `pengguna/${chatWith}`);
 
     const logsRef = databaseRef(database, `logs_pengguna/${chatWith}`);
+   
+    const typingRef = databaseRef(database, `typingStatus/${chatWith}`);
     // Set pengguna online saat masuk
     update(userRef, {
       user: chatWith,
@@ -75,6 +68,17 @@ const ChatPage = () => {
       status: "online",
       timestamp: serverTimestamp(),
     });
+
+   // ✅ Event mengetik otomatis
+    const handleTyping = () => {
+      if (!chatWith) return;
+      update(typingRef, { isTyping: true });
+
+      clearTimeout(typingTimeout);
+      typingTimeout = setTimeout(() => {
+        update(typingRef, { isTyping: false });
+      }, 2000);
+    };
 
     // Set pengguna offline saat keluar
     const handleDisconnect = () => {
