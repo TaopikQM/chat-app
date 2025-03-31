ceed7384c34006c8e1845d1c3c8f3058a7becee6
TaopikQM <taopik61318@gmail.com>
2025-03-31 22:40:32 +0700

    Update page.js

--- src/app/4user/page.js
+++ src/app/4user/page.js
@@ -110,21 +110,21 @@ const ChatPage = () => {
   }, [chatWith]);
   return (
      <div className="max-w-full mx-auto h-screen flex flex-col bg-gray-100">
-      <div className="flex-none p-4 bg-white border-b border-gray-300 shadow-md">
+      <div className="flex-none p-4 bg-white border-b border-gray-300 shadow-md fixed top-0 left-0 w-full z-50">
         <h2 className="text-xl font-semibold text-center">Chat dengan {currentUser}</h2>
         <UserStatus userId={currentUser} />
         
       </div>
 
       {/* Bagian ChatList bisa di-scroll */}
-      <div className="flex-1 overflow-y-auto p-4">
+      <div className="flex-1 overflow-y-auto p-4 mb-16">
         <ChatList user1={chatWith} user2={currentUser} setReplyMessage={setReplyMessage} />
       </div>
 
       
 
       {/* Input tetap di bawah */}
-      <div className="flex-none bg-white border-t border-gray-300">
+      <div className="flex-none bg-white border-t border-gray-300 fixed bottom-0 left-0 w-full">
         <ChatInput pengirim={chatWith} penerima={currentUser} replyMessage={replyMessage} setReplyMessage={setReplyMessage}  
         setIsTyping={setIsTyping}
           />
