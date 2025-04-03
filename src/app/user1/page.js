59aa944a2b0ae50de005dcb60b049dd393fc0619
TaopikQM <taopik61318@gmail.com>
2025-04-03 10:53:26 +0700

    Update page.js

--- src/app/user1/page.js
+++ src/app/user1/page.js
@@ -90,15 +90,16 @@ const ChatPage = () => {
       <div className="flex-none p-4 bg-white border-b border-gray-300 shadow-md  fixed top-0 left-0 w-full z-50">
         <h2 className="text-xl font-semibold text-center">Chat dengan {chatWith}</h2>
         <UserStatus userId={chatWith} />
+        <div className="text-center text-gray-500 text-sm my-2">
+          {isTyping && <span>{chatWith} sedang mengetik...</span>}
+        </div>
         
       </div>
 
       {/* Bagian ChatList bisa di-scroll */}
       <div className="flex-1 overflow-y-auto p-4 mb-16 mt-16">
         <ChatList user1={currentUser} user2={chatWith} setReplyMessage={setReplyMessage} />
-        <div className="text-center text-gray-500 text-sm my-2">
-          {isTyping && <span>{chatWith} sedang mengetik...</span>}
-        </div>
+        
       </div>
 
       {/* Input tetap di bawah */}
