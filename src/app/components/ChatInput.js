ed4f749bce50c5361f179451637255943d51dc25
TaopikQM <taopik61318@gmail.com>
2025-04-04 18:42:47 +0700

    Update ChatInput.js

--- src/app/components/ChatInput.js
+++ src/app/components/ChatInput.js
@@ -400,7 +400,7 @@ console.log("isSendDisabled:", isSendDisabled);
             className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center 
         ${isSendDisabled ? "opacity-50 cursor-not-allowed" : ""}`}
             // disabled={uploading || recording}
-            disabled={isSendDisabled}
+            // disabled={isSendDisabled}
           >
             {uploading ? (
               <>
