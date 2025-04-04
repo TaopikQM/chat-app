2eb0ccd9ede941243b89714d38a9fd39ef9d6f5d
TaopikQM <taopik61318@gmail.com>
2025-04-04 17:55:31 +0700

    Update ChatInput.js

--- src/app/components/ChatInput.js
+++ src/app/components/ChatInput.js
@@ -375,7 +375,22 @@ console.log("isSendDisabled:", isSendDisabled);
           <span className="text-red-400">{recordTime}s</span>
           {/* <button onClick={stopRecording} className="bg-red-500 p-2 rounded-lg">🛑 Stop</button> */}
         </div>
-      ):(
+      ): audioURL && (
+         <div className="relative flex items-center gap-2">
+          <audio controls src={audioURL} className="flex-1"></audio>
+          <button
+            className="absolute -top-2 -right-2 bg-red-500 text-white p-1 rounded-full text-xs"
+            onClick={() => {
+              if (!uploading) { // Hanya bisa diklik jika tidak sedang upload
+                setAudioURL(null);
+                setAudioFile(null);
+              }
+            }}
+          >
+            ❌
+          </button>
+        </div>
+      ) :(
         /* Jika tidak sedang merekam, tampilkan input teks */
         // <input
         //   type="text"
