da886654ac13f6aeb0d6ee920693ded82ca166a2
TaopikQM <taopik61318@gmail.com>
2025-04-04 19:30:07 +0700

    Update ChatInput.js

--- src/app/components/ChatInput.js
+++ src/app/components/ChatInput.js
@@ -402,13 +402,13 @@ console.log("isSendDisabled:", isSendDisabled);
         
       )}
       {!recording&& (
-        
+          <div className={`${isSendDisabled ? "opacity-50 cursor-not-allowed" : ""}`}>
           <button
             onClick={sendMessage}
             className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center 
         ${isSendDisabled ? "opacity-50 cursor-not-allowed" : ""}`}
             // disabled={uploading || recording}
-            disabled={!isSendDisabled}
+            disabled={false}
           >
             {uploading ? (
               <>
@@ -429,6 +429,7 @@ console.log("isSendDisabled:", isSendDisabled);
               "Kirim"
             )}
           </button>
+              </div>
         )}
       </div>
     </div>
