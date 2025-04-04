ee4df4b971593a7d7123fbb4995142107262136b
TaopikQM <taopik61318@gmail.com>
2025-04-04 20:00:49 +0700

    Update page.js

--- src/app/user1/page.js
+++ src/app/user1/page.js
@@ -87,12 +87,14 @@ const ChatPage = () => {
 
   return (
      <div className="max-w-full mx-auto h-screen flex flex-col bg-gray-100">
-      <div className="flex-none p-4 bg-white border-b border-gray-300 shadow-md  fixed top-0 left-0 w-full ">
+      <div className="flex-none bg-white border-b border-gray-300 shadow-md  fixed top-0 left-0 w-full ">
         <h2 className="text-xl font-semibold text-center">Chat dengan {chatWith}</h2>
         <UserStatus userId={chatWith} />
         <div className="text-center text-gray-500 text-sm my-2">
           {isTyping && <span>{chatWith} sedang mengetik...</span>}
         </div>
+  <hr/>
+      <br/>
         
       </div>
 
