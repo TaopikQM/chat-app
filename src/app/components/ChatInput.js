b0665e44a14f7293a6d711a4727e15e015e47113
TaopikQM <taopik61318@gmail.com>
2025-04-04 19:55:19 +0700

    Update ChatInput.js

--- src/app/components/ChatInput.js
+++ src/app/components/ChatInput.js
@@ -409,7 +409,7 @@ const ChatInput = ({ pengirim, penerima , replyMessage, setReplyMessage}) => {
             className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center 
        `}
             // disabled={uploading || recording}
-            disabled={!isSendDisabled}
+            disabled={isSendDisabled}
           >
             {uploading ? (
               <>
