544270c77dce19a59b54856196cb0b0e046d3690
TaopikQM <taopik61318@gmail.com>
2025-04-21 12:40:31 +0700

    Update AdminChatTable.js

--- src/app/components/AdminChatTable.js
+++ src/app/components/AdminChatTable.js
@@ -22,7 +22,7 @@ const AdminChatTable = () => {
 
   //ambil buat nampilin data
   useEffect(() => {
-    const messagesRef = databaseRef(database, "chatsBox");
+    const messagesRef = databaseRef(database, "chatsBox1");
      const usersRef = databaseRef(database, "pengguna");
     const logsChatsRef = databaseRef(database, "log_chatsBox");
     const logsUsersRef = databaseRef(database, "logs_pengguna");
