c5c389ca5ab374a409e2f24e30c4c1a40ccbc7b1
TaopikQM <taopik61318@gmail.com>
2025-05-05 14:28:14 +0700

    Update page.js

--- src/app/2user/page.js
+++ src/app/2user/page.js
@@ -159,7 +159,7 @@ const [gpsEnabled, setGpsEnabled] = useState(false);
       </div>
 
       {/* Bagian ChatList bisa di-scroll */}
-      <div className="flex-1 overflow-y-auto p-4 mb-16 mt-16">
+      <div className="flex-1 overflow-y-auto ">
         <ChatList user1={chatWith} user2={currentUser} setReplyMessage={setReplyMessage} />
        
         
