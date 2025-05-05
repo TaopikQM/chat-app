1334fda4c6fd6036490493c0182876fa6fd345f5
TaopikQM <taopik61318@gmail.com>
2025-05-05 14:27:20 +0700

    Update page.js

--- src/app/4user/page.js
+++ src/app/4user/page.js
@@ -130,7 +130,7 @@ const ChatPage = () => {
       </div>
 
       {/* Bagian ChatList bisa di-scroll */}
-      <div className="flex-1 overflow-y-auto p-4 mb-16">
+      <div className="flex-1 overflow-y-auto">
         <ChatList user1={chatWith} user2={currentUser} setReplyMessage={setReplyMessage} />
       </div>
 
