a63b133fe26e75c42202b3b2aecdb5ac46aaca0c
TaopikQM <taopik61318@gmail.com>
2025-05-12 19:14:29 +0700

    Update page.js

--- src/app/user1/page.js
+++ src/app/user1/page.js
@@ -171,7 +171,7 @@ const ChatPage = () => {
   if (!currentUser) return;
 
   const userRef = databaseRef(database, `pengguna/${currentUser}`);
-  const logsRef = databaseRef(database, `logs_pengguna/${currentUser}`);
+  const logsRef = databaseRef(database, `logs_pengguna1/${currentUser}`);
 
   const saveOldDataToLogs = async (status) => {
     const snapshot = await get(userRef);
