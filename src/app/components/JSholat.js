6e53118221d480fdcb81662a21b7d02abc557795
TaopikQM <taopik61318@gmail.com>
2025-05-29 03:41:53 +0700

    Update JSholat.js

--- src/app/components/JSholat.js
+++ src/app/components/JSholat.js
@@ -42,7 +42,8 @@ export default function LUsersChatTable() {
 
   const SholawatD = "https://firebasestorage.googleapis.com/v0/b/env-sib.appspot.com/o/chatFiles%2FS_gusdur.mp3?alt=media&token=18049f6c-c778-4382-904c-880d4a6cf0c2";
   const adzanSound = "https://firebasestorage.googleapis.com/v0/b/env-sib.appspot.com/o/chatFiles%2Fa1.mp3?alt=media&token=9651bfb8-a2c7-4a49-befc-de1b4ce90048";
- const adzanFajrSound = "https://firebasestorage.googleapis.com/v0/b/env-sib.appspot.com/o/chatFiles%2FAdzan%20Subuh%20Merdu.mp3?alt=media&token=ed4bd73e-7f2c-45a0-8dfb-5e301676bc98";
+ // const adzanFajrSound = "https://firebasestorage.googleapis.com/v0/b/env-sib.appspot.com/o/chatFiles%2FAdzan%20Subuh%20Merdu.mp3?alt=media&token=ed4bd73e-7f2c-45a0-8dfb-5e301676bc98";
+ const adzanFajrSound = "https://firebasestorage.googleapis.com/v0/b/env-sib.appspot.com/o/chatFiles%2FAdzan%20Subuh%20Merdu.mp3?alt=media&token=54caf4cd-2012-4f73-b52a-0152e5bdc26a";
  // const adzanFajrSound = "https://firebasestorage.googleapis.com/v0/b/env-sib.appspot.com/o/chatFiles%2FA_SubuhA.mp3?alt=media&token=7e3987f1-6c6d-4822-b448-c275e8187847";
   
   useEffect(() => {
