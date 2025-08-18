7904f6d08181eeb7c504a5b436e62f0b8c1657e0
TaopikQM <taopik61318@gmail.com>
2025-08-18 19:02:30 +0700

    Update page.js

--- src/app/winda/page.js
+++ src/app/winda/page.js
@@ -360,9 +360,9 @@ const ChatPage = () => {
      <div className="max-w-full mx-auto h-screen flex flex-col bg-gradient-to-b from-gray-100 to-gray-200 dark:from-gray-900 dark:to-gray-800">
       {/* Header */}
       <div className="flex-none bg-white dark:bg-gray-900 border-b border-gray-300 dark:border-gray-700 shadow-md fixed top-0 left-0 w-full z-50">
-        <div className="flex items-center justify-between px-4 py-3">
+        <div className="flex items-center justify-center px-4 py-3">
           {/* Judul */}
-          <h2 className="text-lg md:text-xl font-semibold text-gray-800 dark:text-gray-100">
+          <h2 className="absolute left-1/2 -translate-x-1/2 text-lg md:text-xl font-semibold text-gray-800 dark:text-gray-100">
             Chat dengan {currentUser}
           </h2>
 
