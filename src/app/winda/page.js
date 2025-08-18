773eb3b2d31fa2f85ef96f71ce60f38eb91963e1
TaopikQM <taopik61318@gmail.com>
2025-08-19 06:25:11 +0700

    Update page.js

--- src/app/winda/page.js
+++ src/app/winda/page.js
@@ -360,8 +360,8 @@ const ChatPage = () => {
  <div className="max-w-7xl mx-auto w-full flex flex-col flex-1">
    
      {/*<div className="max-w-full mx-auto h-screen flex flex-col bg-gradient-to-b from-gray-100 to-gray-200 dark:from-gray-900 dark:to-gray-800">
-       Header */}
-      <div className="flex-none bg-white dark:bg-gray-900 border-b border-gray-300 dark:border-gray-700 shadow-md fixed top-0 left-0 w-full z-50">
+       Header fixed top-0 left-0 w-full */}
+      <div className="flex-none bg-white dark:bg-gray-900 border-b border-gray-300 dark:border-gray-700 shadow-md z-50">
         <div className="relative flex items-center justify-center p-2">
           {/* Judul */}
           <h2 className=" text-lg md:text-xl font-semibold text-gray-800 dark:text-gray-100">
@@ -405,8 +405,8 @@ const ChatPage = () => {
         />
       </div>
 
-      {/* Input Chat */}
-      <div className="flex-none bg-white dark:bg-gray-900 border-t border-gray-300 dark:border-gray-700 shadow-md fixed bottom-0 left-0 w-full">
+      {/* Input Chat fixed bottom-0 left-0 w-full*/}
+      <div className="flex-none bg-white dark:bg-gray-900 border-t border-gray-300 dark:border-gray-700 shadow-md ">
         <div className="px-2 sm:px-4 py-2">
           <ChatInput
             pengirim={chatWith}
