692bdb15f1d5d6237d27c486f90a54a00cb93067
TaopikQM <taopik61318@gmail.com>
2025-08-19 06:42:11 +0700

    Update page.js

--- src/app/winda/page.js
+++ src/app/winda/page.js
@@ -356,8 +356,8 @@ const ChatPage = () => {
   };
 }, [chatWith]);
   return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-b from-gray-100 to-gray-200 dark:from-gray-900 dark:to-gray-800">
- <div className="max-w-7xl mx-auto w-full flex flex-col flex-1">
+    <div className="min-h-screen flex flex-col bg-gradient-to-b from-gray-100 to-gray-200 dark:from-gray-900 dark:to-gray-800 border border-gray-900 dark:border-gray-100">
+     <div className="max-w-7xl mx-auto w-full flex flex-col flex-1">
    
      {/*<div className="max-w-full mx-auto h-screen flex flex-col bg-gradient-to-b from-gray-100 to-gray-200 dark:from-gray-900 dark:to-gray-800">
        Header fixed top-0 left-0 w-full */}
