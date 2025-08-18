1265b0add8bdf37b60647ed4ceb95611de033531
TaopikQM <taopik61318@gmail.com>
2025-08-19 05:28:51 +0700

    Update ChatInput.js

--- src/app/components/ChatInput.js
+++ src/app/components/ChatInput.js
@@ -365,74 +365,74 @@ const ChatInput = ({ pengirim, penerima , replyMessage, setReplyMessage, isDark}
         </div>
       )}
       
-      <div className="flex-none gap-3 flex items-center p-4 border-t border-gray-300 sticky bottom-0 bg-white">
+      <div className="flex-none gap-3 flex items-center p-4 border-t border-gray-300 sticky bottom-0 bg-white dark:bg-gray-800 text-gray-900 dark:text-white">
       
-      {/* Jika sedang merekam, tampilkan timer */}
-      <input type="file" multiple onChange={handleFileChange} disabled={uploading|| recording || audioPending} className="hidden" id="fileInput" />
-         <label htmlFor="fileInput"  className={`cursor-pointer ${uploading || recording || audioPending ? "opacity-50 cursor-not-allowed" : ""}`}>
-            <span className="material-icons">📎</span>
-        </label>
-        <button
-          onClick={recording ? stopRecording : startRecording}
-          className={`p-2 rounded-full ${recording ? "bg-red-500" : "bg-blue-500"}`}
-          disabled={uploading}
-        >
-          {recording ? "⏸️" : "🎙️"}
-        </button>
-      {recording ? (
-        <div className="flex items-center gap-2">
-          <span className="text-red-400">{recordTime}s</span>
-          {/* <button onClick={stopRecording} className="bg-red-500 p-2 rounded-lg">🛑 Stop</button> */}
-        </div>
-      ) : (
-        /* Jika tidak sedang merekam, tampilkan input teks */
-        // <input
-        //   type="text"
-        //   className="flex-1 p-2 rounded-lg border "
-        //   placeholder="Ketik pesan..."
-        //   value={newMessage}
-        //   onChange={(e) => setNewMessage(e.target.value)}
-        // />
-        <textarea id="chat" rows="1" className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-900 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-        placeholder="Ketik Pesan..."
-        value={newMessage}
-          // onChange={(e) => setNewMessage(e.target.value)}
-              ref={inputRef}
-                onChange={handleTyping}
-          ></textarea>
-        
-      )}
-      {!recording&& (
-          <div className={`${isSendDisabled ? "opacity-50 cursor-not-allowed" : ""}`}>
-          <button
-            onClick={sendMessage}
-            className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm p-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center 
-       `}
-            // disabled={uploading || recording}
-            disabled={isSendDisabled}
-          >
-            {uploading ? (
-              <>
-                <svg
-                  aria-hidden="true"
-                  role="status"
-                  className="inline w-4 h-4 me-3 text-white animate-spin"
-                  viewBox="0 0 100 101"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
-                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
-                </svg>
-              </>
-            ) : (
-              <svg class="w-6 h-6 text-inherit" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v13m0-13 4 4m-4-4-4 4"/>
-              </svg>
-
-            )}
-          </button>
-              </div>
+          {/* Jika sedang merekam, tampilkan timer */}
+          <input type="file" multiple onChange={handleFileChange} disabled={uploading|| recording || audioPending} className="hidden" id="fileInput" />
+             <label htmlFor="fileInput"  className={`cursor-pointer ${uploading || recording || audioPending ? "opacity-50 cursor-not-allowed" : ""}`}>
+                <span className="material-icons">📎</span>
+            </label>
+            <button
+              onClick={recording ? stopRecording : startRecording}
+              className={`p-2 rounded-full ${recording ? "bg-red-500" : "bg-blue-500"}`}
+              disabled={uploading}
+            >
+              {recording ? "⏸️" : "🎙️"}
+            </button>
+          {recording ? (
+            <div className="flex items-center gap-2">
+              <span className="text-red-400">{recordTime}s</span>
+              {/* <button onClick={stopRecording} className="bg-red-500 p-2 rounded-lg">🛑 Stop</button> */}
+            </div>
+          ) : (
+            /* Jika tidak sedang merekam, tampilkan input teks */
+            // <input
+            //   type="text"
+            //   className="flex-1 p-2 rounded-lg border "
+            //   placeholder="Ketik pesan..."
+            //   value={newMessage}
+            //   onChange={(e) => setNewMessage(e.target.value)}
+            // />
+            <textarea id="chat" rows="1" className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-900 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Ketik Pesan..."
+            value={newMessage}
+              // onChange={(e) => setNewMessage(e.target.value)}
+                  ref={inputRef}
+                    onChange={handleTyping}
+              ></textarea>
+            
+          )}
+          {!recording&& (
+              <div className={`${isSendDisabled ? "opacity-50 cursor-not-allowed" : ""}`}>
+              <button
+                onClick={sendMessage}
+                className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm p-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center 
+           `}
+                // disabled={uploading || recording}
+                disabled={isSendDisabled}
+              >
+                {uploading ? (
+                  <>
+                    <svg
+                      aria-hidden="true"
+                      role="status"
+                      className="inline w-4 h-4 me-3 text-white animate-spin"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
+                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
+                    </svg>
+                  </>
+                ) : (
+                  <svg class="w-6 h-6 text-inherit" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v13m0-13 4 4m-4-4-4 4"/>
+                  </svg>
+    
+                )}
+              </button>
+          </div>
         )}
       </div>
     </div>
@@ -801,6 +801,7 @@ export default ChatInput;
 // // // };
 
 // // // export default ChatInput;
+
 
 
 
