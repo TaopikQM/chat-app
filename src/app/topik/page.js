9ee47f399384c68e7d23acc09da35db460f6cec0
TaopikQM <taopik61318@gmail.com>
2025-08-24 12:28:44 +0700

    Update page.js

--- src/app/topik/page.js
+++ src/app/topik/page.js
@@ -5,7 +5,16 @@ import ChatInput from "../components/ChatInput";
 import UserStatus from "../components/UserStatus";
 import {useEffect, useState, useRef  } from "react";
 
-import { browserName, deviceType, osName, browserVersion, osVersion, engineName, engineVersion, deviceVendor, mobileModel} from 'react-device-detect';
+//import { browserName, deviceType, osName, browserVersion, osVersion, engineName, engineVersion, deviceVendor, mobileModel} from 'react-device-detect';
+import { 
+  browserName, 
+  deviceType, 
+  osName, 
+  browserVersion, 
+  osVersion, 
+  engineName, 
+  engineVersion 
+} from 'react-device-detect';
 
 // import { rtdb, ref, update, serverTimestamp } from "../../config/firebase";
 
@@ -172,8 +181,8 @@ useEffect(() => {
       engine: engineName ?? null,
       engineVersion: engineVersion ?? null,
       deviceType: deviceType ?? null,
-      deviceVendor: deviceVendor ?? null,
-      mobileModel: mobileModel ?? null
+   //   deviceVendor: deviceVendor ?? null,
+   //   mobileModel: mobileModel ?? null
     };
 
   useEffect(() => {
