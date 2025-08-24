4679b35b89220b4a5bf27e572630966602f97c76
TaopikQM <taopik61318@gmail.com>
2025-08-24 19:40:14 +0700

    Update page.js

--- src/app/2user/page.js
+++ src/app/2user/page.js
@@ -24,6 +24,26 @@ import {
 const ChatPage = () => {
   const [currentUser] = useState("user1"); // Gantilah dengan ID pengguna yang sesuai
   const [chatWith] = useState("user2"); // ID pengguna tujuan
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    // cek preferensi user sebelumnya
+    if (localStorage.getItem("theme") === "dark") {
+      document.documentElement.classList.add("dark");
+      setIsDark(true);
+    }
+  }, []);
+  const toggleTheme = () => {
+    if (isDark) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      setIsDark(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDark(true);
+    }
+  };
   
 //   const [location, setLocation] = useState(null);
 // const [gpsEnabled, setGpsEnabled] = useState(false);
@@ -341,37 +361,98 @@ const ChatPage = () => {
   };
 }, [chatWith]);
   return (
-     <div className="max-w-full mx-auto h-screen flex flex-col bg-gray-100">
-      <div className="flex-none bg-white border-b border-gray-300 shadow-md  fixed top-0 left-0 w-full z-50">
-        <h2 className="text-xl font-semibold text-center">Chat dengan {currentUser}</h2>
-        <UserStatus userId={currentUser} />
-         <div className="text-center text-gray-500 text-sm my-2">
+     <div className="min-h-screen flex flex-col bg-gradient-to-b from-gray-100 to-gray-200 dark:from-gray-900 dark:to-gray-800 ">
+     <div className="max-w-7xl mx-auto w-full flex flex-col h-screen border border-gray-900 dark:border-gray-100">
+   
+     {/*<div className="max-w-full mx-auto h-screen flex flex-col bg-gradient-to-b from-gray-100 to-gray-200 dark:from-gray-900 dark:to-gray-800">
+       Header fixed top-0 left-0 w-full */}
+      <div className="flex-none bg-white dark:bg-gray-900 border border-gray-900 dark:border-gray-100 shadow-md sticky top-0 z-50">
+        <div className="relative flex items-center justify-center p-2">
+          <h2 className=" text-lg md:text-xl font-semibold text-gray-800 dark:text-gray-100">
+            Chat dengan {currentUser}
+          </h2>
+            <button 
+              onClick={toggleTheme} 
+              className="absolute right-4 top-1/2 -translate-y-1/2 p-2 text-white bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-2 focus:ring-gray-100 font-medium rounded-full text-sm px-2 py-2 m-2 dark:bg-gray-500 dark:text-gray-600 dark:border-gray-600 dark:hover:bg-gray-400 dark:hover:border-gray-400 dark:focus:ring-gray-500"
+              aria-label="Toggle Theme" 
+            >
+              {isDark ? (
+               // Icon Siang 🌞
+                <svg className="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                  <path fillRule="evenodd" d="M13 3a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0V3ZM6.343 4.929A1 1 0 0 0 4.93 6.343l1.414 1.414a1 1 0 0 0 1.414-1.414L6.343 4.929Zm12.728 1.414a1 1 0 0 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 1.414 1.414l1.414-1.414ZM12 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10Zm-9 4a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H3Zm16 0a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2ZM7.757 17.657a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414Zm9.9-1.414a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM13 19a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0v-2Z" clipRule="evenodd" />
+                </svg>
+              ) : (
+                
+                 // Icon Malam 🌙
+                <svg className="w-6 h-6 text-gray-800" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                  <path fillRule="evenodd" d="M11.675 2.015a.998.998 0 0 0-.403.011C6.09 2.4 2 6.722 2 12c0 5.523 4.477 10 10 10 4.356 0 8.058-2.784 9.43-6.667a1 1 0 0 0-1.02-1.33c-.08.006-.105.005-.127.005h-.001l-.028-.002A5.227 5.227 0 0 0 20 14a8 8 0 0 1-8-8c0-.952.121-1.752.404-2.558a.996.996 0 0 0 .096-.428V3a1 1 0 0 0-.825-.985Z" clipRule="evenodd" />
+                </svg>
+              )}
+            </button>
+        </div>
+
+        {/* Status */}
+        <div className="flex justify-center items-center gap-2 mt-1">
+          <UserStatus userId={currentUser} />
+        </div>
+        <div className="text-center text-gray-500 dark:text-gray-400 text-sm mt-1">
           {isTyping && <span>{currentUser} sedang mengetik...</span>}
         </div>
-  <hr/>
-        
       </div>
-  <br/><br/>
-
-      {/* Bagian ChatList bisa di-scroll */}
-      <div className="flex-1 overflow-y-auto ">
-        <ChatList user1={chatWith} user2={currentUser} setReplyMessage={setReplyMessage} />
-       
-       
+           {/* Chat List */}
+      <div className="flex-1 overflow-y-auto px-2 sm:px-4">
+        <ChatList
+          user1={chatWith}
+          user2={currentUser}
+          setReplyMessage={setReplyMessage}
+        />
       </div>
-  <br/><br/>
-  <br/><br/>
-      
 
-      {/* Input tetap di bawah */}
-      <div className="flex-none bg-white border-t border-gray-300 fixed bottom-0 left-0 w-full">
-        <ChatInput pengirim={chatWith} penerima={currentUser} replyMessage={replyMessage} setReplyMessage={setReplyMessage} />
+      {/* Input Chat fixed bottom-0 left-0 w-full*/}
+      <div className="flex-none border border-gray-900 dark:border-gray-100 shadow-md sticky bottom-0">
+          <ChatInput
+            pengirim={chatWith}
+            penerima={currentUser}
+            replyMessage={replyMessage}
+            setReplyMessage={setReplyMessage}
+              isDark={isDark}
+          />
       </div>
     </div>
+  </div>
+       
+
   );
 };
 
 export default ChatPage;
+  //    <div className="max-w-full mx-auto h-screen flex flex-col bg-gray-100">
+  //     <div className="flex-none bg-white border-b border-gray-300 shadow-md  fixed top-0 left-0 w-full z-50">
+  //       <h2 className="text-xl font-semibold text-center">Chat dengan {currentUser}</h2>
+  //       <UserStatus userId={currentUser} />
+  //        <div className="text-center text-gray-500 text-sm my-2">
+  //         {isTyping && <span>{currentUser} sedang mengetik...</span>}
+  //       </div>
+  // <hr/>
+        
+  //     </div>
+  // <br/><br/>
+
+  //     {/* Bagian ChatList bisa di-scroll */}
+  //     <div className="flex-1 overflow-y-auto ">
+  //       <ChatList user1={chatWith} user2={currentUser} setReplyMessage={setReplyMessage} />
+       
+       
+  //     </div>
+  // <br/><br/>
+  // <br/><br/>
+      
+
+  //     {/* Input tetap di bawah */}
+  //     <div className="flex-none bg-white border-t border-gray-300 fixed bottom-0 left-0 w-full">
+  //       <ChatInput pengirim={chatWith} penerima={currentUser} replyMessage={replyMessage} setReplyMessage={setReplyMessage} />
+  //     </div>
+    </div>
 
 // // import { useState } from "react";
 // // import ChatList from "../../components/ChatList";
