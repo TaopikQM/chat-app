0a71b5cccea7f025c7eda96b957cfa9391a2fe19
TaopikQM <taopik61318@gmail.com>
2025-08-24 22:15:10 +0700

    Update page.js

--- src/app/AdminChatTable/page.js
+++ src/app/AdminChatTable/page.js
@@ -50,7 +50,9 @@ export default function AdminPage() {
        Header fixed top-0 left-0 w-full */}
       <div className="flex-none bg-white dark:bg-gray-900 border border-gray-900 dark:border-gray-100 shadow-md sticky top-0 z-50">
         <div className="relative flex items-center justify-center p-2">
-         
+         <h2 className=" text-lg md:text-xl font-semibold text-gray-800 dark:text-gray-100">
+            DATA CHAT ADMIN
+          </h2>
             <button 
               onClick={toggleTheme} 
               className="absolute right-4 top-1/2 -translate-y-1/2 p-2 text-white bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-2 focus:ring-gray-100 font-medium rounded-full text-sm px-2 py-2 m-2 dark:bg-gray-500 dark:text-gray-600 dark:border-gray-600 dark:hover:bg-gray-400 dark:hover:border-gray-400 dark:focus:ring-gray-500"
