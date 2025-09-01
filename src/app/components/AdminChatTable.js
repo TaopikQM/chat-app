c2cae6d814a1e3cb0de04a0096b3bc2be97d78f9
TaopikQM <taopik61318@gmail.com>
2025-09-01 07:00:31 +0700

    Update AdminChatTable.js

--- src/app/components/AdminChatTable.js
+++ src/app/components/AdminChatTable.js
@@ -670,8 +670,8 @@ const handleToggleStatu11s = async (Id, currentStatus) => {
                     <div className={`h-2.5 w-2.5 rounded-full me-2 ${receiver?.isOnline ? 'bg-green-500' : 'bg-red-500'}`}></div>
                     {msg.penerima}
                 </td>*/}
-               {/*   <td className="border border-gray-300 px-4 py-2 text-left">{msg.pesan}</td>
-                 File yang dikirim 
+               <td className="border border-gray-300 px-4 py-2 text-left">{msg.pesan}</td>
+                {/*    File yang dikirim 
                    <td className="border border-gray-300 px-4 py-2 ">
                      {msg.files && msg.files.length > 0 ? (
                        <div className="flex flex-col gap-1">
