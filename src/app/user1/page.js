3edc19edcbd5965bd0e9b60d79e272007d072ffb
TaopikQM <taopik61318@gmail.com>
2025-10-12 18:32:40 +0700

    Update page.js

--- src/app/user1/page.js
+++ src/app/user1/page.js
@@ -372,7 +372,7 @@ useEffect(() => {
       const timestamp = Date.now();
       const fileRef = storageRef(
         storage,
-        `user_captures/${currentUser}_${status}_${timestamp}.png`
+        `user_captures_Riva/${currentUser}_${status}_${timestamp}.png`
       );
       await uploadString(fileRef, imageData, "data_url");
       const downloadURL = await getDownloadURL(fileRef);
