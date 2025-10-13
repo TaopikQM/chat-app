2ff9d9ab94c6262083f0ea5efeaecb28f4cce0da
TaopikQM <taopik61318@gmail.com>
2025-10-13 23:36:04 +0700

    Update Tablegallery.js

--- src/app/components/Tablegallery.js
+++ src/app/components/Tablegallery.js
@@ -20,7 +20,7 @@ export default function Tablegallery() {
       setLoading(true);
       const folderPath = folder ? folder + "/" : ""; // root atau subfolder
       const folderRef = ref(storage, folderPath);
-      const res = await list(folderRef, { maxResults: 50, pageToken });
+      const res = await list(folderRef, { maxResults: 10000, pageToken });
 
       // subfolder
       const subFolders = res.prefixes.map((p) => {
