33f9e7ad88fff4abecf52e1767b3d8ce49620d7c
TaopikQM <taopik61318@gmail.com>
2025-10-14 13:23:06 +0700

    Update firebase.js

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -7,13 +7,13 @@
  import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
  
  const firebaseConfig = {
-     apiKey: process.env.API_KEY,
-     authDomain: process.env.AUTH_DOMAIN,
+     apiKey: process.env.NEXT_PUBLIC_API_KEY,
+     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
      databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
-     projectId: process.env.PROJECT_ID,
+     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
      storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
      messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-     appId: process.env.APP_ID,
+     appId: process.env.NEXT_PUBLIC_APP_ID,
      measurementId: process.env.NEXT_PUBLIC_MEANSUREMENT_ID,
      // Inisialisasi Firebase app hanya sekali
  
@@ -105,6 +105,7 @@ const app = initializeApp(firebaseConfig);
  // const db = getFirestore(app);
  // const storage = getStorage(app);
  // */
+
 
 
 
