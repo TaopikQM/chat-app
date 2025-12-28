42ea92319add08c6adae216ae92bc26eda5a5b70
TaopikQM <taopik61318@gmail.com>
2025-12-28 12:32:45 +0700

    Create presence.js

---
+++ src/app/lib/presence.js
@@ -0,0 +1,50 @@
+import {
+  ref,
+  onValue,
+  set,
+  update,
+  onDisconnect,
+  serverTimestamp,
+} from "firebase/database";
+import { database } from "./firebase";
+
+export const setupPresence = (userId, page) => {
+  const userRef = ref(database, `statusOnlineweb/${userId}`);
+  const connectedRef = ref(database, ".info/connected");
+
+  onValue(connectedRef, (snap) => {
+    if (snap.val() === true) {
+      onDisconnect(userRef).set({
+        isOnline: false,
+        lastSeen: serverTimestamp(),
+        page,
+      });
+
+      set(userRef, {
+        isOnline: true,
+        lastSeen: serverTimestamp(),
+        page,
+      });
+    }
+  });
+
+  const handleVisibility = () => {
+    if (document.visibilityState === "hidden") {
+      update(userRef, {
+        isOnline: false,
+        lastSeen: serverTimestamp(),
+      });
+    } else {
+      update(userRef, {
+        isOnline: true,
+        lastSeen: serverTimestamp(),
+      });
+    }
+  };
+
+  document.addEventListener("visibilitychange", handleVisibility);
+
+  return () => {
+    document.removeEventListener("visibilitychange", handleVisibility);
+  };
+};
