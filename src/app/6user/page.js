3a04c92446acfe7487fe895ffba50e1f1f4c3a87
TaopikQM <taopik61318@gmail.com>
2025-12-28 18:05:58 +0700

    Update page.js

--- src/app/6user/page.js
+++ src/app/6user/page.js
@@ -178,20 +178,13 @@ const [gpsEnabled, setGpsEnabled] = useState(false);
       <div className="flex-none bg-white border-b border-gray-300 shadow-md  fixed top-0 left-0 w-full z-50">
         <h2 className="text-xl font-semibold text-center">Chat dengan {currentUser}</h2>
    <h2 className="text-xl font-semibold text-center">{currentUser}</h2>
-                <div>
-            {targetStatus.isOnline &&
-              <p className="text-xs text-gray-500">
-                Terakhir dilihat {formatLastSeen(targetStatus.lastSeen, now)}
-              </p>}
-                </div>
-
-                <span
-                  className={`text-sm ${targetStatus.isOnline
-                    ? "text-green-600"
-                    : "text-red-600"}`}
-                >
-                  {targetStatus.isOnline ? "ONLINE" : "OFFLINE"}
-                </span>
+                <p className="text-xs text-gray-500">
+                   {targetStatus?.isOnline
+                    ? "ONLINE"
+                    : targetStatus?.lastSeen
+                    ? `Terakhir online ${formatLastSeen(targetStatus.lastSeen, now)}`
+                    : "OFFLINE"}
+                </p>
                   
         <UserStatus userId={currentUser} />
          <div className="text-center text-gray-500 text-sm my-2">
