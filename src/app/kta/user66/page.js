0b0e2b091fef28acbb0ab7b5d0a028cc3786c5ce
TaopikQM <taopik61318@gmail.com>
2025-12-30 10:30:35 +0700

    Update and rename page.js to page.js

--- src/app/kta/user66/page.js
+++ src/app/kta/user66/page.js
@@ -5,19 +5,19 @@ import { setupPresence } from "../../lib/presence";
 import UserStatus from "../../components/UserStatus";
 
 export default function KTAUser6() {
-  const currentUser = "user666";
-  const targetUser = "user555";
+  const currentUser = "user66";
+  const targetUser = "user55";
 
   useEffect(() => {
-    return setupPresence(currentUser, "/kta/user6");
+    return setupPresence(currentUser, "/kta/user66");
   }, []);
 
   return (
     <div className="p-6">
-      <h1 className="font-bold text-xl">KTA USER 6</h1>
+      <h1 className="font-bold text-xl">KTA USER 66</h1>
 
       <div className="mt-4">
-        <p className="font-semibold">Status user5:</p>
+        <p className="font-semibold">Status user55:</p>
         <UserStatus userId={targetUser} />
       </div>
     </div>
