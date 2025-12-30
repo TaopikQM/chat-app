e04aa8c308a4aad3b5012aa5faa0b7e3ef18b052
TaopikQM <taopik61318@gmail.com>
2025-12-30 09:22:59 +0700

    Update useNow.js

--- src/app/hooks/useNow.js
+++ src/app/hooks/useNow.js
@@ -1,29 +1,46 @@
+// import { useEffect, useState } from "react";
+
+// let listeners = [];
+// let intervalStarted = false;
+
+// function startGlobalTimer() {
+//   if (intervalStarted) return;
+
+//   intervalStarted = true;
+//   setInterval(() => {
+//     const now = Date.now();
+//     listeners.forEach((l) => l(now));
+//   }, 1000);
+// }
+
+// export function useNow() {
+//   const [now, setNow] = useState(Date.now());
+
+//   useEffect(() => {
+//     startGlobalTimer();
+
+//     listeners.push(setNow);
+
+//     return () => {
+//       listeners = listeners.filter((l) => l !== setNow);
+//     };
+//   }, []);
+
+//   return now;
+// }
+
+
+
+
+"use client";
 import { useEffect, useState } from "react";
-
-let listeners = [];
-let intervalStarted = false;
-
-function startGlobalTimer() {
-  if (intervalStarted) return;
-
-  intervalStarted = true;
-  setInterval(() => {
-    const now = Date.now();
-    listeners.forEach((l) => l(now));
-  }, 1000);
-}
 
 export function useNow() {
   const [now, setNow] = useState(Date.now());
 
   useEffect(() => {
-    startGlobalTimer();
-
-    listeners.push(setNow);
-
-    return () => {
-      listeners = listeners.filter((l) => l !== setNow);
-    };
+    const i = setInterval(() => setNow(Date.now()), 1000);
+    return () => clearInterval(i);
   }, []);
 
   return now;
