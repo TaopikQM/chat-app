0a9d983c1a2d943555a680abfdb0ab491bf6d455
TaopikQM <taopik61318@gmail.com>
2026-01-03 08:21:19 +0700

    Update route.js

--- src/app/api/uploadPhotosp/route.js
+++ src/app/api/uploadPhotosp/route.js
@@ -1,6 +1,6 @@
 // File: src/app/api/uploadPhoto/route.js
 import { NextResponse } from "next/server";
-import { getDatabase, ref, push, set } from "firebase/database";
+import { getDatabase,  ref as databaseRef, push, set } from "firebase/database";
 import { ref, uploadString, getDownloadURL } from "firebase/storage";
 import { storage, storageBackup,database } from "../../config/firebase"; // sesuaikan path
 
@@ -51,7 +51,7 @@ export async function POST(request) {
 
     const downloadURL = await getDownloadURL(fileRef);
 
-     const photoRef =push(ref(database,  `${year}/${month}/${day}/fotosp/${currentUser}`));
+     const photoRef =push(databaseRef(database,  `${year}/${month}/${day}/fotosp/${currentUser}`));
     await set(photoRef, {
       photoURL: downloadURL,
       imageData,
