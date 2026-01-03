de2071e9cc95121c74d47c713eb395512d556029
TaopikQM <taopik61318@gmail.com>
2026-01-03 13:29:17 +0700

    Update page.js

--- src/app/tabelG/page.js
+++ src/app/tabelG/page.js
@@ -10,7 +10,7 @@
 //   onValue,
 //   get,
 // } from "firebase/database";
-// import { database } from "../config/firebase";
+import { database } from "../config/firebase";
 
 // // import { database } from "../config/firebase";
 
@@ -177,7 +177,7 @@ import {
   endBefore,
   get,
 } from "firebase/database";
-import { database } from "@/lib/firebase";
+// import { database } from "@/lib/firebase";
 
 const PAGE_SIZE = 20;
 
