1fb1ea9da5eef6c1e627c05efd4801bbd20c668d
TaopikQM <taopik61318@gmail.com>
2026-01-03 08:54:16 +0700

    Update route.js

--- src/app/api/uploadPhotod/route.js
+++ src/app/api/uploadPhotod/route.js
@@ -146,8 +146,9 @@
 
 // File: src/app/api/uploadPhoto/route.js
 import { NextResponse } from "next/server";
+import { getDatabase,  ref as databaseRef, push, set } from "firebase/database";
 import { ref, uploadString, getDownloadURL } from "firebase/storage";
-import { storage, storageBackup } from "../../config/firebase"; // sesuaikan path
+import { storage, storageBackup,database } from "../../config/firebase"; // sesuaikan path
 
 export async function POST(request) {
   try {
@@ -195,6 +196,19 @@ export async function POST(request) {
     }
 
     const downloadURL = await getDownloadURL(fileRef);
+       const photoRef =push(databaseRef(database,  `${year}/${month}/${day}/fotodh/${currentUser}`));
+    await set(photoRef, {
+      photoURL: downloadURL,
+      imageData,
+      filePath,
+      facing: facing || "unknown",
+      status,
+      diah,
+      chatWith: chatWith || null,
+      currentUser:currentUser||null,
+      storageUsed,
+      createdAt: timestamp,
+    });
 
     return NextResponse.json({
       code: 200,
