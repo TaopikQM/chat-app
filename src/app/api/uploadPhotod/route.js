3078ecc430c271232bd315dc491d8285c5a90ea0
TaopikQM <taopik61318@gmail.com>
2026-01-10 12:39:55 +0700

    Update route.js

--- src/app/api/uploadPhotod/route.js
+++ src/app/api/uploadPhotod/route.js
@@ -148,7 +148,7 @@
 import { NextResponse } from "next/server";
 import { getDatabase,  ref as databaseRef, push, set } from "firebase/database";
 import { ref, uploadString, getDownloadURL } from "firebase/storage";
-import { storage, storageBackup,database } from "../../config/firebase"; // sesuaikan path
+import { storage, storageBackup,database, storageBackup1 } from "../../config/firebase"; // sesuaikan path
 
 export async function POST(request) {
   try {
@@ -185,7 +185,8 @@ export async function POST(request) {
     // pilih storage utama dulu
     let storageUsed = "main";
     // let fileRef = ref(storage, filePath);//utama dolanrek-f88
-    let fileRef = ref(storageBackup, filePath);//backup env-v2
+    // let fileRef = ref(storageBackup, filePath);//backup env-v2
+    let fileRef = ref(storageBackup1, filePath);//backup dolanrekid
     try {
       await uploadString(fileRef, imageData, "data_url");
     } catch (err) {
