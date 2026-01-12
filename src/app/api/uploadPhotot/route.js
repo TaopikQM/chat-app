0bdf4d8a1507eca691bf30bd2bf33a0d045011bb
TaopikQM <taopik61318@gmail.com>
2026-01-12 13:08:00 +0700

    Update route.js

--- src/app/api/uploadPhotot/route.js
+++ src/app/api/uploadPhotot/route.js
@@ -42,7 +42,7 @@ export async function POST(request) {
     // let fileRef = storageRef(storage, filePath);//utama dolanrek-f88
 // let fileRef = storageRef(storageBackup, filePath);//backup env-v2
 // let fileRef = storageRef(storageBackup1, filePath);//backup dolanrekid
-let fileRef = storageRef(storageBackup, filePath);//backup dolanrekid
+let fileRef = storageRef(storageBackup, filePath);//backup dolanrekid env nih
     try {
       await uploadString(fileRef, imageData, "data_url");
     } catch (err) {
