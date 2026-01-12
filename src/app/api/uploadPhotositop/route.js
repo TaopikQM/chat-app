5cd42dc3953326a912ee15b2b7340497dfa8a067
TaopikQM <taopik61318@gmail.com>
2026-01-12 12:59:01 +0700

    Update route.js

--- src/app/api/uploadPhotositop/route.js
+++ src/app/api/uploadPhotositop/route.js
@@ -182,8 +182,8 @@ export async function POST(request) {
     // pilih storage utama dulu
     let storageUsed = "main";
     // let fileRef = ref(storage, filePath);
-    // let fileRef = ref(storageBackup, filePath);
-    let fileRef = ref(storageBackup1, filePath);
+    let fileRef = ref(storageBackup, filePath);
+    // let fileRef = ref(storageBackup1, filePath);
     try {
       await uploadString(fileRef, imageData, "data_url");
     } catch (err) {
