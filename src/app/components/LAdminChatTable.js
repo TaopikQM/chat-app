2da9594ed79b3229b338a7d4ccab9fac4ae8a61a
TaopikQM <taopik61318@gmail.com>
2026-01-20 01:58:20 +0700

    Update LAdminChatTable.js

--- src/app/components/LAdminChatTable.js
+++ src/app/components/LAdminChatTable.js
@@ -111,46 +111,64 @@ const LAdminChatTable = () => {
 
     useEffect(() => {
               const logsUsersRef= databaseRef(database, "log_chatsBox");
-      
-              const unsubscribe = onValue(logsUsersRef, (snapshot) => {
-                  const data = snapshot.val();
-                  if (!data) {
-                  setLogsChats([]);
-                  return;
-                  }
-      
-                  let finalLogs = [];
-      
-                  Object.keys(data).forEach((userId) => {
-                  const userLogsObj = data[userId];
-      
-                  // ubah ke array
-                  const userLogs = Object.entries(userLogsObj)
-                      .map(([logId, log]) => ({
-                      id: logId,
-                      userId,
-                      ...log,
-                      }))
-                  //     // 🔹 urutkan terbaru → lama
-                  //     .sort((a, b) => b.deleteTime - a.deleteTime)
-                  //     // 🔹 AMBIL HANYA 20 TERBARU
-                  //     .slice(0, 20);
-      
-                  // // gabung ke global array
-                  finalLogs.push(...userLogs);
-                  });
-      
-                  // (opsional) sort global kalau mau campur semua user
-                  // finalLogs.sort((a, b) => b.deleteTime - a.deleteTime);
+        const data = snapshot.val();
 
-                               // 🔥 POTONG SETELAH DIGABUNG
-                  finalLogs = finalLogs
-                    .sort((a, b) => b.deleteTime - a.deleteTime)
-                    .slice(0, 20); // ← TOTAL 20
+    if (!data) {
+      setLogsUsers([]);
+      return;
+    }
+
+    const sortedData = Object.entries(data)
+      .map(([id, value]) => ({
+        id,
+        ...value,
+      }))
+      .sort((a, b) => b.deleteTime - a.deleteTime)
+      .slice(0, 50); // 🔹 LIMIT 150
+
+    setLogsUsers(sortedData);
+    console.log("logs data user (limit 50):", sortedData);
+  });
+      
+              // const unsubscribe = onValue(logsUsersRef, (snapshot) => {
+              //     const data = snapshot.val();
+              //     if (!data) {
+              //     setLogsChats([]);
+              //     return;
+              //     }
+      
+              //     let finalLogs = [];
+      
+              //     Object.keys(data).forEach((userId) => {
+              //     const userLogsObj = data[userId];
+      
+              //     // ubah ke array
+              //     const userLogs = Object.entries(userLogsObj)
+              //         .map(([logId, log]) => ({
+              //         id: logId,
+              //         userId,
+              //         ...log,
+              //         }))
+              //     //     // 🔹 urutkan terbaru → lama
+              //     //     .sort((a, b) => b.deleteTime - a.deleteTime)
+              //     //     // 🔹 AMBIL HANYA 20 TERBARU
+              //     //     .slice(0, 20);
+      
+              //     // // gabung ke global array
+              //     finalLogs.push(...userLogs);
+              //     });
+      
+              //     // (opsional) sort global kalau mau campur semua user
+              //     // finalLogs.sort((a, b) => b.deleteTime - a.deleteTime);
+
+              //                  // 🔥 POTONG SETELAH DIGABUNG
+              //     finalLogs = finalLogs
+              //       .sort((a, b) => b.deleteTime - a.deleteTime)
+              //       .slice(0, 20); // ← TOTAL 20
                 
-                  setLogsChats(finalLogs);
-                  console.log("Logs messages per user (limit 20):", finalLogs);
-              });
+              //     setLogsChats(finalLogs);
+              //     console.log("Logs messages per user (limit 20):", finalLogs);
+              // });
       
               return () => unsubscribe();
           }, []);
@@ -2107,6 +2125,7 @@ export default LAdminChatTable;
 // // // // // // // // };
 
 // // // // // // // // export default AdminChatTable;
+
 
 
 
