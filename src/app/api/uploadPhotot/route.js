b148247987a8c8b3946b488d75f10a4d773eb4a9
TaopikQM <taopik61318@gmail.com>
2026-03-09 13:52:47 +0700

    Modify storage references in uploadPhoto route
    Updated storage references to include additional storage options for image uploads.

--- src/app/api/uploadPhotot/route.js
+++ src/app/api/uploadPhotot/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 import { getDatabase,  ref as databaseRef, push, set } from "firebase/database";
 import { ref as storageRef, uploadString, getDownloadURL } from "firebase/storage";
-import { storage, storageBackup,database , storageBackup1} from "../../config/firebase"; // sesuaikan path
+import { storage, storageBackup,database , storageBackup1, storageapotek1, storageuas13256,  storageuas_firebase_a0256, storageproa112113270} from "../../config/firebase"; // sesuaikan path
 
 export async function POST(request) {
   try {
@@ -41,14 +41,15 @@ export async function POST(request) {
     let storageUsed = "main";
     // let fileRef = storageRef(storage, filePath);//utama dolanrek-f88
 // let fileRef = storageRef(storageBackup, filePath);//backup env-v2
-let fileRef = storageRef(storageBackup1, filePath);//backup dolanrekid
+// let fileRef = storageRef(storageBackup1, filePath);//backup dolanrekid
+let fileRef = storageRef(storageapotek1, filePath);//backup dolanrekid
 // let fileRef = storageRef(storageBackup, filePath);//backup dolanrekid env nih
     try {
       await uploadString(fileRef, imageData, "data_url");
     } catch (err) {
       console.warn("Upload ke storage utama gagal, fallback ke backup:", err);
       storageUsed = "backup";
-      fileRef = ref(storageBackup, filePath);
+      fileRef = ref(storageuas13256, filePath);
       await uploadString(fileRef, imageData, "data_url");
     }
 
