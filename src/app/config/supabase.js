d2f6db5a07bf510d7e334af6878a6fca35d776e4
TaopikQM <taopik61318@gmail.com>
2026-03-20 17:01:55 +0700

    Add Supabase client configuration

---
+++ src/app/config/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  https://tldxduzaosaifpxibcxx.supabase.co,
+  sb_publishable_pzlikSJDba1PyeZiw8B0sQ_ccWcbNml
+);
