4e652214106b55cf839ca3f916cc98d90046083b
TaopikQM <taopik61318@gmail.com>
2026-03-22 22:21:33 +0700

    Update Supabase client initialization with env variables

--- src/app/config/supabase.js
+++ src/app/config/supabase.js
@@ -1,6 +1,15 @@
+// import { createClient } from "@supabase/supabase-js";
+
+// export const supabase = createClient(
+//   https://tldxduzaosaifpxibcxx.supabase.co,
+//   sb_publishable_pzlikSJDba1PyeZiw8B0sQ_ccWcbNml
+// );
+
+
+
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  https://tldxduzaosaifpxibcxx.supabase.co,
-  sb_publishable_pzlikSJDba1PyeZiw8B0sQ_ccWcbNml
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY
 );
