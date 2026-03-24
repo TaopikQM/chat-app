04a8d8f79b71c23223f7bcf4bea603f7601715d5
TaopikQM <taopik61318@gmail.com>
2026-03-25 05:32:07 +0700

    Update page.js

--- src/app/components/gallerysub/page.js
+++ src/app/components/gallerysub/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-(* import { supabase } from "@/config/supabase"; *)
+//(* import { supabase } from "@/config/supabase"; *)
 
 import { supabase } from  "../config/supabase";
 
