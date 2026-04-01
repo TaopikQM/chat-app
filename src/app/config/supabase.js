cef2e2cc7ab601bc1bf636a38e2e0ce8a3d9a3a4
TaopikQM <taopik61318@gmail.com>
2026-04-01 13:31:57 +0700

    Update supabase.js

--- src/app/config/supabase.js
+++ src/app/config/supabase.js
@@ -3,16 +3,16 @@
 // export const supabase = createClient(
 //   https://tldxduzaosaifpxibcxx.supabase.co,
 //   sb_publishable_pzlikSJDba1PyeZiw8B0sQ_ccWcbNml
-// );
+// ); process.env.NEXT_PUBLIC_SUPABASE_URL,
+  // process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY,
+
 
 
 
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY,
-
+ 
   process.env.NEXT_PUBLIC_SUPABASE_URL_B,
   process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY_B
   
