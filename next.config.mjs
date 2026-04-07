89e8f589317b11570f07f9682b8e4736a5a2b07f
TaopikQM <taopik61318@gmail.com>
2026-04-07 15:44:47 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   images: {
+    domains: ['https://kzfrgcwdvufwtjoanmkl.supabase.co'], // domain supabase kamu
+  },
+  
+};
 
 export default nextConfig;
