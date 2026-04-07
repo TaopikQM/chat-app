40890bdf324c523d82731a96c895a961e8bb406f
TaopikQM <taopik61318@gmail.com>
2026-04-07 10:06:06 +0700

    Update supabase.js

--- src/app/config/supabase.js
+++ src/app/config/supabase.js
@@ -13,7 +13,12 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY
+   process.env.NEXT_PUBLIC_SUPABASE_URL1,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY1
+  
+);
+export const supabase1 = createClient(
    process.env.NEXT_PUBLIC_SUPABASE_URL1,
   process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY1
   
