0537528b05ab7a835f58a04922362dec6455e7b7
TaopikQM <taopik61318@gmail.com>
2026-04-07 15:58:59 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    images: {
-    domains: ['https://kzfrgcwdvufwtjoanmkl.supabase.co'], // domain supabase kamu
+    domains: ['kzfrgcwdvufwtjoanmkl.supabase.co'], // domain supabase kamu
   },
   
 };
