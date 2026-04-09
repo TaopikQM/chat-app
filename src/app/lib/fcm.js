5ec01fb30bd85990359b1288b4981704f628b7c0
TaopikQM <taopik61318@gmail.com>
2026-04-09 23:27:43 +0700

    Create fcm.js

---
+++ src/app/lib/fcm.js
@@ -0,0 +1,56 @@
+
+
+"use client";
+// import { initFCM } from "@/lib/fcm";
+// import { getMessaging, getToken } from "../config/firebase/messaging";
+import { app, database, getMessaging, getToken } from "@/config/firebase";
+import { ref, set } from "firebase/database";
+
+export async function initFCM(userId) {
+  try {
+    // 🔥 cek browser support
+    if (typeof window === "undefined") return;
+
+    const messaging = getMessaging(app);
+
+    // 🔥 minta izin notif
+    const permission = await Notification.requestPermission();
+
+    if (permission !== "granted") {
+      console.log("❌ Notif tidak diizinkan");
+      return;
+    }
+
+    // 🔥 ambil token
+    const token = await getToken(messaging, {
+      vapidKey: "BBiuf9a4Q4j75ggkXu-oSJ2ywJZhQL-D01V0V3RdOK4sQ449WDmXo11Km1MTTF5eioVgPg4B_SGhzhDWEhAW580";,
+    });
+
+    if (!token) {
+      console.log("❌ Token tidak didapat");
+      return;
+    }
+
+    console.log("✅ FCM Token:", token);
+
+    // 🔥 device ID unik
+    let deviceId = localStorage.getItem("deviceId");
+    if (!deviceId) {
+      deviceId = crypto.randomUUID();
+      localStorage.setItem("deviceId", deviceId);
+    }
+
+    // 🔥 simpan ke realtime database
+    await set(ref(database, `usersDevices/${userId}/${deviceId}`), {
+      fcmToken: token,
+      notificationPermission: permission,
+      isActive: true,
+      onChatPage: true,
+      lastSeen: Date.now(),
+    });
+
+    console.log("✔ Token disimpan ke Firebase");
+  } catch (error) {
+    console.error("❌ FCM ERROR:", error);
+  }
+}
