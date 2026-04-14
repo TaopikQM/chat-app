0dde025e3699fc9d5742d4d19a7412fb68fad8ae
TaopikQM <taopik61318@gmail.com>
2026-04-14 11:07:21 +0700

    Update page.js

--- src/app/tpk/page.js
+++ src/app/tpk/page.js
@@ -767,6 +767,9 @@ try {
           <h2 className=" text-lg md:text-xl font-semibold text-gray-800 dark:text-gray-100">
             Chat dengan {chatWith}
           </h2>
+  <button onClick={() => initFCM(currentUser)}>
+  Aktifkan Notif
+</button>
             <button 
               onClick={toggleTheme} 
               className="absolute right-4 top-1/2 -translate-y-1/2 p-2 text-white bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-2 focus:ring-gray-100 font-medium rounded-full text-sm px-2 py-2 m-2 dark:bg-gray-500 dark:text-gray-600 dark:border-gray-600 dark:hover:bg-gray-400 dark:hover:border-gray-400 dark:focus:ring-gray-500"
