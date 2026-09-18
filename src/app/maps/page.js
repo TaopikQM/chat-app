df9b1b850964ca7279947459a539b4addf1ed332
TaopikQM <taopik61318@gmail.com>
2026-09-18 08:54:21 +0700

    Comment out MapView and Home component in page.js
    Comment out dynamic import of MapView and Home component.

--- src/app/maps/page.js
+++ src/app/maps/page.js
@@ -1,11 +1,11 @@
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 
-const MapView = dynamic(() => import("../components/MapView"), { ssr: false });
+// const MapView = dynamic(() => import("../components/MapView"), { ssr: false });
 
-export default function Home() {
-  return (
-    <>
-      <MapView />
-    </>
-  );
-}
+// export default function Home() {
+//   return (
+//     <>
+//       <MapView />
+//     </>
+//   );
+// }
