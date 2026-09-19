7b2631e97b94076275411e864a5c841195650e36
TaopikQM <taopik61318@gmail.com>
2026-09-19 10:26:26 +0700

    Comment out CDN URL in Supabase configuration
    Comment out the NEXT_PUB_CDN_URL environment variable.

--- src/app/config/supabase.js
+++ src/app/config/supabase.js
@@ -8,13 +8,14 @@
 
   // process.env.NEXT_PUBLIC_SUPABASE_URL,
 
+  // process.env.NEXT_PUB_CDN_URL,
+
 
 
 
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.NEXT_PUB_CDN_URL,
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY
   
